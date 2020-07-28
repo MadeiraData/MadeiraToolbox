025e032ab7cb4107a217ab8b3ac3ada965d74bbe
Eitan Blumin <eitan@madeiradata.com>
2020-07-28 09:42:56 +0300

    Added CaptureTSQLErrors via Extended Events Buffer

--- Monitoring Scripts/CaptureTSQLErrors_XE_buffer.sql
+++ Monitoring Scripts/CaptureTSQLErrors_XE_buffer.sql
@@ -1,6 +1,8 @@
 -- Author: Eitan Blumin (t: @EitanBlumin | b: eitanblumin.com)
 -- Date: 2020-05-31
+-- Last Update: 2020-07-15
 -- Description: Collect T-SQL Error Events using an Extended Events Buffer
+--		The script automatically detects whether you're in an Azure SQL DB, or a regular SQL Server instance.
 
 SET NOCOUNT ON;
 
@@ -10,28 +12,43 @@ DECLARE
 , @BufferMaxMemoryMB INT
 , @BufferMaxEventsCount INT
 , @BufferXESessionName SYSNAME
-, @PrintOnly BIT
+, @UseFileTarget BIT
+, @FileTargetMaxFiles INT
+, @FileTargetMaxFileSizeMB INT
+, @FileTargetPath NVARCHAR(4000)
 , @FlushBuffer BIT
+, @PrintOnly BIT
 
 DECLARE @ProgramsToIgnore AS TABLE(appname SYSNAME);
 
-/******************** CONFIGURATION ********************/
+/********************* CONFIGURATION *********************/
 
+SET @BufferXESessionName	= 'CaptureTSQLErrors'	-- Name of the extended events session
 SET @SourceLinkedServer		= NULL			-- Optionally place a linked server name here. Set as NULL to monitor the local server.
-SET @MinimumSeverity		= 11			-- Set minimum threshold of error severity to capture
+SET @MinimumSeverity		= 11			-- Set minimum threshold of error severity to capture.
+SET @PrintOnly			= 0			-- Set to 1 to get the command in text as output, instead of running it
+SET @UseFileTarget		= 1			-- Set to 1 to use file target, otherwise, the ring buffer will be used
+
+/*************** Ring Buffer Configuration ***************/
+SET @FlushBuffer		= 1			-- Set to 1 to flush the ring buffer between executions by recreating the XE session. Otherwise, keep data.
 SET @BufferMaxMemoryMB		= 16			-- Max memory in MB for the ring buffer
 SET @BufferMaxEventsCount	= 50000			-- Max number of events to hold between flushes
-SET @BufferXESessionName	= 'CaptureTSQLErrors'	-- Name of the extended events session
-SET @FlushBuffer		= 1			-- Set to 1 to flush buffer between executions, 0 to keep data
-SET @PrintOnly			= 0			-- Set to 1 to get the command in text as output, instead of running it
 
+/*************** File Target Configuration ***************/
+SET @FileTargetPath		= NULL			-- Set the file target path (required in Azure SQL DB to use Blob Storage URL). Leave as NULL to use default SQL Server LOG folder.
+SET @FileTargetMaxFiles		= 5			-- Max number of rollover file target files
+SET @FileTargetMaxFileSizeMB	= 20			-- Max size in MB of each file target file
+
+/****************** Programs to Exclude ******************/
 INSERT INTO @ProgramsToIgnore
 SELECT 'Microsoft SQL Server Management Studio - Transact-SQL IntelliSense'
+UNION ALL SELECT 'Microsoft SQL Server Management Studio - Query'
+UNION ALL SELECT 'Microsoft SQL Server Management Studio'
 UNION ALL SELECT 'SQLServerCEIP'
 UNION ALL SELECT 'check_mssql_health'
 UNION ALL SELECT 'SQL Server Performance Investigator'
 
-/******************** /CONFIGURATION ********************/
+/********************* /CONFIGURATION *********************/
 
 DECLARE @CMD NVARCHAR(MAX), @Filters NVARCHAR(MAX), @ProcFilters NVARCHAR(MAX), @Executor NVARCHAR(1000)
 DECLARE @IsAzureSQLDB BIT, @IsNestedTransaction BIT
@@ -41,11 +58,30 @@ SET @CMD = N'SET @IsAzureSQLDB = CASE WHEN SERVERPROPERTY(''Edition'') = ''SQL A
 
 EXEC @Executor @CMD, N'@IsAzureSQLDB BIT OUTPUT', @IsAzureSQLDB OUTPUT;
 
-SET @Filters = N'([package0].[equal_boolean]([sqlserver].[is_system],(0)))
-AND [severity]>=(' + CONVERT(nvarchar(max),@MinimumSeverity) + N') AND [sqlserver].[sql_text]<>N''''
-'
+IF @IsAzureSQLDB = 1 AND @UseFileTarget = 1 AND (@FileTargetPath IS NULL OR @FileTargetPath NOT LIKE 'https://%')
+BEGIN
+	RAISERROR(N'When using File Target in Azure SQL DB, you must specify a valid Azure Blog Storage URL in @FileTargetPath.',11,1);
+	RAISERROR('Switching over to ring buffer target.',0,1) WITH NOWAIT;
+	SET @UseFileTarget = 0;
+END
 
-SELECT @Filters = @Filters + CHAR(10) + N'AND (sqlserver.client_app_name <> ''' + REPLACE(appname, N'''', N'''''') + N''')'
+-- Add xel file postfix if missing
+IF @FileTargetPath IS NOT NULL AND @UseFileTarget = 1
+BEGIN
+	IF RIGHT(@FileTargetPath, 1) IN ('/','\') SET @FileTargetPath = @FileTargetPath + @BufferXESessionName + '.xel'
+	IF RIGHT(@FileTargetPath, 4) <> '.xel' SET @FileTargetPath = @FileTargetPath + '.xel'
+END
+
+IF @UseFileTarget = 1 AND @FlushBuffer = 1
+	PRINT N'WARNING: Using File Target. Buffer will NOT be flushed!'
+ELSE
+	SET @UseFileTarget = ISNULL(@UseFileTarget, 0);
+
+SET @Filters = CONCAT(N'([package0].[equal_boolean]([sqlserver].[is_system],(0)))
+AND [severity]>=(' ,@MinimumSeverity, N') AND [sqlserver].[sql_text]<>N''''
+')
+
+SELECT @Filters = @Filters + CHAR(10) + N'AND (sqlserver.client_app_name <> ' + QUOTENAME(appname, N'''') + N')'
 FROM @ProgramsToIgnore
 
 SetUpCommand:
@@ -63,14 +99,24 @@ SET @CMD = N'-- Retrieve buffer contents
 IF OBJECT_ID(''tempdb..#events'') IS NOT NULL DROP TABLE #events
 CREATE TABLE #events (event_xml XML);
 INSERT INTO #events
-SELECT X.query(''.'')
+' + CASE WHEN @UseFileTarget = 1 THEN 
+N'SELECT xdata = CAST(event_data AS xml)
+FROM (
+select [TargetFileName] = REPLACE(c.column_value, ''.xel'', ''*.xel'')
+from sys.dm_xe_' + CASE WHEN @IsAzureSQLDB = 1 THEN N'database_' ELSE N'' END + N'sessions AS s
+join sys.dm_xe_' + CASE WHEN @IsAzureSQLDB = 1 THEN N'database_' ELSE N'' END + N'session_object_columns AS c ON s.address = c.event_session_address
+where column_name = ''filename'' and s.name = ' + QUOTENAME(@BufferXESessionName, N'''') + N'
+) AS FileTarget CROSS APPLY sys.fn_xe_file_target_read_file (FileTarget.TargetFileName,null,null, null)'
+ELSE
+N'SELECT X.query(''.'')
 FROM (SELECT xdata = CAST(xet.target_data AS xml)
 FROM sys.dm_xe_' + CASE WHEN @IsAzureSQLDB = 1 THEN N'database_' ELSE N'' END + N'session_targets AS xet  
-JOIN sys.dm_xe_' + CASE WHEN @IsAzureSQLDB = 1 THEN N'database_' ELSE N'' END + N'sessions AS xe ON (xe.address = xet.event_session_address)  
-WHERE xe.name = ''' + @BufferXESessionName + N'''
+JOIN sys.dm_xe_' + CASE WHEN @IsAzureSQLDB = 1 THEN N'database_' ELSE N'' END + N'sessions AS xe ON xe.address = xet.event_session_address
+WHERE xe.name = ' + QUOTENAME(@BufferXESessionName, N'''') + N'
 AND target_name= ''ring_buffer''
 ) AS a
-CROSS APPLY xdata.nodes (N''//event'') AS session_events (X)
+CROSS APPLY xdata.nodes (N''//event'') AS session_events (X)'
+END + N'
 
 -- Unfurl raw data
 SELECT
@@ -97,12 +143,21 @@ END
 ELSE
 BEGIN
 SET @CMD = N'-- Retrieve buffer contents
-SELECT CAST(xet.target_data AS varbinary(max))
+' + CASE WHEN @UseFileTarget = 1 THEN 
+N'SELECT xdata = CAST(event_data AS varbinary(max))
+FROM (
+select [TargetFileName] = REPLACE(c.column_value, ''.xel'', ''*.xel'')
+from sys.dm_xe_' + CASE WHEN @IsAzureSQLDB = 1 THEN N'database_' ELSE N'' END + N'sessions AS s
+join sys.dm_xe_' + CASE WHEN @IsAzureSQLDB = 1 THEN N'database_' ELSE N'' END + N'session_object_columns AS c ON s.address = c.event_session_address
+where column_name = ''filename'' and s.name = ' + QUOTENAME(@BufferXESessionName, N'''') + N'
+) AS FileTarget CROSS APPLY sys.fn_xe_file_target_read_file (FileTarget.TargetFileName,null,null, null)'
+  ELSE
+N'SELECT xdata = CAST(xet.target_data AS varbinary(max))
 FROM sys.dm_xe_' + CASE WHEN @IsAzureSQLDB = 1 THEN N'database_' ELSE N'' END + N'session_targets AS xet  
-JOIN sys.dm_xe_' + CASE WHEN @IsAzureSQLDB = 1 THEN N'database_' ELSE N'' END + N'sessions AS xe ON (xe.address = xet.event_session_address)  
-WHERE xe.name = ''' + @BufferXESessionName + N'''
-AND target_name= ''ring_buffer''
-'
+JOIN sys.dm_xe_' + CASE WHEN @IsAzureSQLDB = 1 THEN N'database_' ELSE N'' END + N'sessions AS xe ON xe.address = xet.event_session_address
+WHERE xe.name = ' + QUOTENAME(@BufferXESessionName, N'''') + N'
+AND target_name= ''ring_buffer'''
+  END
 END
 
 IF @FlushBuffer = 1
@@ -135,8 +190,16 @@ sqlserver.sql_text
 )
 WHERE ' + @Filters + ISNULL(@ProcFilters, N'') + N'
 )
-
-ADD TARGET package0.ring_buffer(SET max_events_limit=(' + CONVERT(nvarchar, @BufferMaxEventsCount) + N'),max_memory=(' + CONVERT(nvarchar, @BufferMaxMemoryMB*1024) + N'))
+' + CASE WHEN @UseFileTarget = 1 THEN
+CONCAT(
+N'ADD TARGET package0.event_file(SET filename=N', QUOTENAME(ISNULL(@FileTargetPath, @BufferXESessionName + '.xel'), N'''')
+, N',max_file_size=(', @FileTargetMaxFileSizeMB , N'),max_rollover_files=(', @FileTargetMaxFiles, N'))'
+)
+ELSE
+CONCAT(
+N'ADD TARGET package0.ring_buffer(SET max_events_limit=(', @BufferMaxEventsCount, N'),max_memory=(', (@BufferMaxMemoryMB*1024), N'))'
+)
+END + N'
 WITH (MAX_MEMORY=' + CONVERT(nvarchar, @BufferMaxMemoryMB) + N' MB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=30 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=PER_CPU,TRACK_CAUSALITY=OFF,STARTUP_STATE=ON)
 ;
 END
@@ -175,10 +238,15 @@ END CATCH
 
 IF OBJECT_ID('tempdb..#events') IS NOT NULL DROP TABLE #events
 CREATE TABLE #events (event_xml XML);
-INSERT INTO #events
-SELECT X.query('.')
-FROM (SELECT xdata = CONVERT(xml, xdata) FROM #xe) AS a
-CROSS APPLY xdata.nodes (N'//event') AS session_events (X)
+
+IF @UseFileTarget = 1
+	INSERT INTO #events
+	SELECT xdata = CONVERT(xml, xdata) FROM #xe
+ELSE
+	INSERT INTO #events
+	SELECT X.query('.')
+	FROM (SELECT xdata = CONVERT(xml, xdata) FROM #xe) AS a
+	CROSS APPLY xdata.nodes (N'//event') AS session_events (X)
 
 -- Unfurl raw data
 SELECT
