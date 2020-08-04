6bfd9a66c666d3b80badb2878cf2a7eb427c64b2
Eitan Blumin <eitan@madeiradata.com>
2020-08-04 12:14:02 +0300

    added @SpaceUsedMaxPercent parameter, updated indentations and comments

--- Utility Scripts/TempDB_Sizing_Check_and_Remediation.sql
+++ Utility Scripts/TempDB_Sizing_Check_and_Remediation.sql
@@ -9,20 +9,26 @@ the disk volume where the TempDB files are located.
 This check only works when TempDB files are isolated from other databases and exist on their own dedicated volume.
 
 Change log:
-2020-08-04 - Added @ClearServerCache parameter
+2020-08-04 - Added @ClearServerCache and @SpaceUsedMaxPercent parameters, updated indentations and comments
 2020-03-08 - Bug fixes, added parameters for more control
 2020-01-19 - Removed max size setting, replaced with UNLIMITED. Added @FileGrowthMB as parameter.
 2019-11-19 - First version
 -----------------------------------
 */
 
+DECLARE @ClearServerCache 		BIT	= 0 -- If shrinking tempdb files doesn't work, you may have to set this to 1 to clear objects from server cache.
+DECLARE @MaxSizeDiskUtilizationPercent 	FLOAT 	= 95 -- Maximum total percentage to be used for MAXSIZE property. Use NULL for UNLIMITED.
+DECLARE @InitSizeDiskUtilizationPercent FLOAT 	= 70 -- Desired total percentage of disk space to be used for SIZE property.
+DECLARE @InitialSizeMBOverride 		INT 	= NULL -- 2048 -- hard-coded size per file for edge cases, overriding the calculation based on disk size.
+DECLARE @FileGrowthMB 			INT 	= 1024 -- Auto-growth increment in MB.
+DECLARE @IncludeTransactionLog 		BIT 	= 0 -- Specify whether to include the transaction log file in the calculations.
+DECLARE @SpaceUsedMaxPercent 		INT 	= 50 -- Maximum percent space used compared to desired file size, if found to be above - a warning will be raised.
+
+/** DO NOT CHANGE ANYTHING BELOW THIS LINE **/
+
 SET NOCOUNT, XACT_ABORT, ARITHABORT ON;
-DECLARE @ClearServerCache BIT = 0 -- if shrinking tempdb files doesn't work, you may have to set this to 1 to clear objects from server cache
-DECLARE @MaxSizeDiskUtilizationPercent FLOAT = 95 -- use NULL for UNLIMITED
-DECLARE @InitSizeDiskUtilizationPercent FLOAT = 70
-DECLARE @InitialSizeMBOverride INT = NULL -- 2048 -- hard-coded number override for edge cases
-DECLARE @FileGrowthMB INT = 1024
-DECLARE @IncludeTransactionLog BIT = 0
+USE [tempdb];
+
 DECLARE @CMDs AS TABLE (CMD nvarchar(max));
 
 IF @ClearServerCache = 1
@@ -42,13 +48,15 @@ SELECT
  Remediation_Script = 
  CASE WHEN current_size_MB > InitSizeMBPerFile THEN N'USE tempdb; DBCC SHRINKFILE (N' + QUOTENAME(name, '''') + ' , ' + CONVERT(nvarchar,InitSizeMBPerFile) + N'); '
  ELSE N'' END
-+ 'ALTER DATABASE tempdb MODIFY FILE (NAME = ' + QUOTENAME(name, '''') + ', SIZE = ' + CONVERT(nvarchar,ISNULL(@InitialSizeMBOverride, InitSizeMBPerFile)) + N'MB, MAXSIZE = ' + ISNULL(CONVERT(nvarchar,MaxSizeMBPerFile) + 'MB','UNLIMITED') + N', FILEGROWTH = ' + CONVERT(nvarchar, @FileGrowthMB) + N'MB);'
++ 'ALTER DATABASE [tempdb] MODIFY FILE (NAME = ' + QUOTENAME(name, '''') + ', SIZE = ' + CONVERT(nvarchar,ISNULL(@InitialSizeMBOverride, InitSizeMBPerFile)) + N'MB, MAXSIZE = ' + ISNULL(CONVERT(nvarchar,MaxSizeMBPerFile) + 'MB','UNLIMITED') + N', FILEGROWTH = ' + CONVERT(nvarchar, @FileGrowthMB) + N'MB);'
++ CONCAT('-- Current size: ', current_size_MB, ' MB, Space Used: ', current_spaceused_MB, ' MB (', ROUND(current_spaceused_MB * 1.0 / InitSizeMBPerFile * 100,2), ' % of desired size)')
++ CASE WHEN current_spaceused_MB * 1.0 / InitSizeMBPerFile > @SpaceUsedMaxPercent / 100.0 THEN ' !!WARNING!!' ELSE '' END
 --, *
 FROM
 (
 	SELECT 
-		  CEILING(@MaxSizeDiskUtilizationPercent / 100.0 * total_MB_ceil / TotalNumOfFiles) AS MaxSizeMBPerFile
-		 , FLOOR(@InitSizeDiskUtilizationPercent / 100.0 * total_MB_floor / TotalNumOfFiles) AS InitSizeMBPerFile
+		  MaxSizeMBPerFile = CEILING(@MaxSizeDiskUtilizationPercent / 100.0 * total_MB_ceil / TotalNumOfFiles)
+		, InitSizeMBPerFile = FLOOR(@InitSizeDiskUtilizationPercent / 100.0 * total_MB_floor / TotalNumOfFiles)
 		, *
 	FROM
 	(
@@ -57,9 +65,10 @@ FROM
 	, current_size_MB = size * 8 / 1024
 	, current_maxsize_MB = max_size * 8 / 1024
 	, current_growth_MB = growth * 8 / 1024
-	, CEILING(B.total_bytes / 1024 / 1024) AS total_MB_ceil
-	, FLOOR(B.total_bytes / 1024 / 1024) AS total_MB_floor
-	, COUNT(*) OVER () AS TotalNumOfFiles
+	, current_spaceused_MB = FILEPROPERTY(A.[name], 'SpaceUsed') / 128
+	, total_MB_ceil = CEILING(B.total_bytes / 1024 / 1024)
+	, total_MB_floor = FLOOR(B.total_bytes / 1024 / 1024)
+	, TotalNumOfFiles = COUNT(*) OVER ()
 	, A.[name]
 	, A.[type]
 	, A.is_percent_growth
@@ -108,3 +117,7 @@ END
 
 
 SELECT CMD AS Remediation_Script FROM @CMDs
+
+IF EXISTS (SELECT * FROM @CMDs WHERE CMD LIKE '%DBCC SHRINKFILE%--% !!WARNING!!')
+	RAISERROR(N'WARNING! One or more files have a high space used compared to the desired file size!
+Please try to reduce TempDB workload, and/or increase the desired file size percentage.', 16,1);
