8a038f8602c413f780283148606f20a2a3f708be
Eitan Blumin <eitan@madeiradata.com>
2020-11-10 10:54:18 +0200

    added SampleRatePercent parameter, fixed indentations, added MAXDOP 1 to query

--- Best Practices Checklists/Outdated_Statistics.sql
+++ Best Practices Checklists/Outdated_Statistics.sql
@@ -1,60 +1,71 @@
 DECLARE
-   @MinimumTableRows INT = 200000
- , @MinimumModCountr INT = 100000
- , @MinimumDaysOld INT = 35
- , @MaxDOP INT = NULL -- set to 1 to reduce server workload
+   @MinimumTableRows	INT = 200000
+ , @MinimumModCountr	INT = 100000
+ , @MinimumDaysOld	INT = 35
+ , @MaxDOP		INT = NULL -- set to 1 to reduce server workload
+ , @SampleRatePercent	INT = NULL -- set to number between 1 and 100 to force a specific sample rate, where 100 = FULLSCAN
 
 SET NOCOUNT, ARITHABORT, XACT_ABORT ON;
 IF OBJECT_ID('tempdb..#tmpStats') IS NOT NULL DROP TABLE #tmpStats;
 CREATE TABLE #tmpStats(
- DBname SYSNAME,
- DatabaseId INT,
- ObjectId INT,
- StatsName SYSNAME,
- LastUpdate DATETIME,
- ModCntr BIGINT,
- TotalRows BIGINT
+  DBname SYSNAME
+, DatabaseId INT
+, ObjectId INT
+, StatsName SYSNAME
+, LastUpdate DATETIME
+, ModCntr BIGINT
+, TotalRows BIGINT
 );
 
-DECLARE @qry NVARCHAR(MAX);
-SET @qry = N'
- SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+DECLARE @qry NVARCHAR(MAX), @options NVARCHAR(MAX);
 
- INSERT #tmpStats
- SELECT
-  DB_NAME(),
-  DB_ID(),
-  stat.object_id,
-  stat.name,
-  MIN(sp.last_updated),
-  MAX(sp.modification_counter),
-  SUM(ps.rows)
- FROM sys.tables AS t
+IF @MaxDOP IS NOT NULL
+	SET @options = ISNULL(@options + N', ', N' WITH ') + N'MAXDOP = ' + CONVERT(nvarchar, @MaxDOP)
+
+IF @SampleRatePercent = 100
+	SET @options = ISNULL(@options + N', ', N' WITH ') + N'FULLSCAN'
+ELSE IF @SampleRatePercent IS NOT NULL
+	SET @options = ISNULL(@options + N', ', N' WITH ') + N'SAMPLE ' + CONVERT(nvarchar, @SampleRatePercent) + N' PERCENT'
+
+SET @qry = N'
+  SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+  
+  INSERT #tmpStats
+  SELECT
+    DB_NAME(),
+    DB_ID(),
+    stat.object_id,
+    stat.name,
+    MIN(sp.last_updated),
+    MAX(sp.modification_counter),
+    SUM(ps.rows)
+  FROM sys.tables AS t
   INNER JOIN (
-   SELECT SUM(ps.rows) AS rows, ps.object_id
-   FROM sys.partitions ps 
-   WHERE ps.index_id <= 1 
-   GROUP BY ps.object_id
-   HAVING SUM(ps.rows) >= ' + CONVERT(nvarchar, @MinimumTableRows) + N'
-   ) AS ps
-   ON t.object_id = ps.object_id 
+     SELECT SUM(ps.rows) AS rows, ps.object_id
+     FROM sys.partitions ps 
+     WHERE ps.index_id <= 1 
+     GROUP BY ps.object_id
+     HAVING SUM(ps.rows) >= ' + CONVERT(nvarchar, @MinimumTableRows) + N'
+     ) AS ps
+     ON t.object_id = ps.object_id 
   INNER JOIN sys.stats AS stat ON t.object_id = stat.object_id
   CROSS APPLY
-  (
-  SELECT modification_counter, last_updated
-  FROM sys.dm_db_stats_properties(stat.object_id, stat.stats_id)
-  WHERE modification_counter >= ' + CONVERT(nvarchar, @MinimumModCountr) + N'
-  AND last_updated < DATEADD(day, -' + CONVERT(nvarchar, @MinimumDaysOld) + N', GETDATE())
-  '
-  + CASE WHEN OBJECT_ID('sys.dm_db_incremental_stats_properties') IS NULL THEN N'' ELSE 
-  N'UNION ALL
-  SELECT modification_counter, last_updated
-  FROM sys.dm_db_incremental_stats_properties(stat.object_id, stat.stats_id)
-  WHERE modification_counter >= ' + CONVERT(nvarchar, @MinimumModCountr) + N'
-  AND last_updated < DATEADD(day, -' + CONVERT(nvarchar, @MinimumDaysOld) + N', GETDATE())
-  ' END
+    (
+    SELECT modification_counter, last_updated
+    FROM sys.dm_db_stats_properties(stat.object_id, stat.stats_id)
+    WHERE modification_counter >= ' + CONVERT(nvarchar, @MinimumModCountr) + N'
+    AND last_updated < DATEADD(day, -' + CONVERT(nvarchar, @MinimumDaysOld) + N', GETDATE())
+    '
+    + CASE WHEN OBJECT_ID('sys.dm_db_incremental_stats_properties') IS NULL THEN N'' ELSE 
+    N'UNION ALL
+    SELECT modification_counter, last_updated
+    FROM sys.dm_db_incremental_stats_properties(stat.object_id, stat.stats_id)
+    WHERE modification_counter >= ' + CONVERT(nvarchar, @MinimumModCountr) + N'
+    AND last_updated < DATEADD(day, -' + CONVERT(nvarchar, @MinimumDaysOld) + N', GETDATE())
+    ' END
   + N') AS sp
- GROUP BY stat.object_id,stat.name'
+  GROUP BY stat.object_id,stat.name
+  OPTION (MAXDOP 1)'
 
 IF CONVERT(varchar(300),SERVERPROPERTY('Edition')) = 'SQL Azure'
 BEGIN
@@ -85,9 +96,9 @@ SELECT
 	+ N'.' + QUOTENAME(OBJECT_SCHEMA_NAME(objectId, databaseId)) COLLATE database_default
 	+ N'.' + QUOTENAME(OBJECT_NAME(objectId, databaseId)) COLLATE database_default
 	+ N' ' + QUOTENAME(statsName) COLLATE database_default
-	+ CASE WHEN @MaxDOP IS NULL THEN N'' ELSE N' WITH MAXDOP = ' + CONVERT(nvarchar, @MaxDOP) END
+	+ ISNULL(@options, N'')
 	+ N';'
 FROM #tmpStats
 ORDER BY
-	ModCntr DESC,
-	LastUpdate ASC
+  ModCntr DESC
+, LastUpdate ASC
