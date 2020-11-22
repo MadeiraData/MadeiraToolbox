5b519f20558c64c0da56cbd75cdb7cc07d06e91f
Guy Glantser <guy@madeiradata.com>
2020-11-22 12:33:11 +0200

    Update Collect Instance Information.sql
    Added columns from sys.dm_os_host_info and from sys.dm_os_sys_info.

--- Database Maintenance Plans/Collect Instance Information.sql
+++ Database Maintenance Plans/Collect Instance Information.sql
@@ -1,17 +1,17 @@
 /*========================================================================================================================
 
-Description:	Display information about the instance to be used for establishing a database maintenance plan
+Description:	Display general information about the SQL Server instance and the host platform
 Scope:			Instance
 Author:			Guy Glantser
 Created:		09/09/2020
-Last Updated:	09/09/2020
-Notes:			Use this information to plan a maintenance plan for the user databases in the instance
+Last Updated:	22/11/2020
+Notes:			N/A
 
 =========================================================================================================================*/
 
 SELECT
-	ServerName			= SERVERPROPERTY ('ServerName') ,
-	InstanceVersion		=
+	ServerName					= SERVERPROPERTY ('ServerName') ,
+	InstanceVersion				=
 		CASE
 			WHEN CAST (SERVERPROPERTY ('ProductVersion') AS NVARCHAR(128)) LIKE '8%'	THEN N'2000'
 			WHEN CAST (SERVERPROPERTY ('ProductVersion') AS NVARCHAR(128)) LIKE '9%'	THEN N'2005'
@@ -23,10 +23,19 @@ SELECT
 			WHEN CAST (SERVERPROPERTY ('ProductVersion') AS NVARCHAR(128)) LIKE '14%'	THEN N'2017'
 			WHEN CAST (SERVERPROPERTY ('ProductVersion') AS NVARCHAR(128)) LIKE '15%'	THEN N'2019'
 		END ,
-	ProductLevel		= SERVERPROPERTY ('ProductLevel') ,
-	ProductUpdateLevel	= SERVERPROPERTY ('ProductUpdateLevel') ,	-- Applies to: SQL Server 2012 (11.x) through current version in updates beginning in late 2015
-	ProductBuildNumber	= SERVERPROPERTY ('ProductVersion') ,
-	InstanceEdition		= SERVERPROPERTY ('Edition') ,
-	IsClustered			= SERVERPROPERTY ('IsClustered') ,
-	IsHadrEnabled		= SERVERPROPERTY ('IsHadrEnabled');			-- Applies to: SQL Server 2012 (11.x) and later
+	ProductLevel				= SERVERPROPERTY ('ProductLevel') ,
+	ProductUpdateLevel			= SERVERPROPERTY ('ProductUpdateLevel') ,										-- Applies to: SQL Server 2012 (11.x) through current version in updates beginning in late 2015
+	ProductBuildNumber			= SERVERPROPERTY ('ProductVersion') ,
+	InstanceEdition				= SERVERPROPERTY ('Edition') ,
+	IsClustered					= SERVERPROPERTY ('IsClustered') ,
+	IsHadrEnabled				= SERVERPROPERTY ('IsHadrEnabled') ,											-- Applies to: SQL Server 2012 (11.x) and later
+	HostPlatform				= HostInfo.host_distribution ,													-- Applies to: SQL Server 2017 (14.x) and later
+	VirtualizationType			= SystemInfo.virtual_machine_type_desc ,										-- Applies to: SQL Server 2008 R2 and later
+	NumberOfCores				= SystemInfo.cpu_count ,
+	PhysicalMemory_GB			= CAST (ROUND (SystemInfo.physical_memory_kb / 1024.0 / 1024.0 , 0) AS INT) ,	-- Applies to: SQL Server 2012 (11.x) and later
+	LastServiceRestratDateTime	= SystemInfo.sqlserver_start_time
+FROM
+	sys.dm_os_host_info AS HostInfo
+CROSS JOIN
+	sys.dm_os_sys_info AS SystemInfo;
 GO
