829a722d88c051084551b86b0a37a4392aeac315
Eitan Blumin <eitan@madeiradata.com>
2020-12-06 19:07:58 +0200

    Create simple_sample.ps1

---
+++ High Availability Solutions/Compare-AGReplicaJobs/simple_sample.ps1
@@ -0,0 +1,13 @@
+﻿
+$ComputerName = "."
+$outputFolder = ""
+$emailFrom = "sql_alerts@acme.com"
+$emailTo = @("dba@acme.com","it@acme.com")
+$emailServerAddress = "smtp.acme.com"
+
+
+Import-Module .\Compare-AGReplicaJobs.psd1;
+
+Compare-AGReplicaJobs -From $emailFrom -To $emailTo -EmailServer $emailServerAddress -ComputerName $ComputerName -outputFolder $outputFolder -Verbose
+
+Remove-Module Compare-AGReplicaJobs
