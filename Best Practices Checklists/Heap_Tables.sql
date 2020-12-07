01bc6f750371d88336d14d8d56af48efc85aa710
Eitan Blumin <eitan@madeiradata.com>
2020-12-07 11:28:53 +0200

    Update Heap_Tables.sql

--- Best Practices Checklists/Heap_Tables.sql
+++ Best Practices Checklists/Heap_Tables.sql
@@ -2,17 +2,25 @@ SET NOCOUNT, ARITHABORT, XACT_ABORT ON;
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 
 DECLARE @SqlStatement VARCHAR(max) = ''
+DECLARE @Results AS TABLE
+(
+	DatabaseName SYSNAME NULL,
+	SchemaName SYSNAME NULL,
+	TableName SYSNAME NULL,
+	NumberOfRows INT NULL,
+	SampleDate DATETIME NULL
+);
 
 SELECT @SqlStatement += '
 	SELECT DatabaseName = ' + QUOTENAME([name], N'''') + '
-	,SchemaName = OBJECT_SCHEMA_NAME(object_id)
-	,TableName = OBJECT_NAME(object_id)
+	,SchemaName = OBJECT_SCHEMA_NAME(object_id, ' + CONVERT(nvarchar(max), database_id) + N')
+	,TableName = OBJECT_NAME(object_id, ' + CONVERT(nvarchar(max), database_id) + N')
 	,NumberOfRows = (
 		SELECT SUM(rows)
 		FROM ' + QUOTENAME([name]) + '.sys.partitions AS p
 		WHERE p.object_id = i.object_id AND p.index_id = i.index_id
 		)
-		,SampleDate = GETDATE()
+	,SampleDate = GETDATE()
 FROM ' + QUOTENAME([name]) + '.sys.indexes AS i
 WHERE index_id = 0' + CHAR(10)
 FROM sys.databases
@@ -22,7 +30,11 @@ AND state = 0
 AND DATABASEPROPERTYEX([name], 'Updateability') = 'READ_WRITE'
 
 --PRINT (@SqlStatement)
+
+INSERT INTO @Results
 EXEC (@SqlStatement)
+
+SELECT * FROM @Results;
 GO
 
 
