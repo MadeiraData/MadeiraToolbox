90c11e747f07d58d6e7d025cc7a0884f9a08b4bd
Eitan Blumin <eitan@madeiradata.com>
2021-02-08 18:39:17 +0200

    Added time-based naming to the file names as well

--- Utility Scripts/create_database_snapshot.sql
+++ Utility Scripts/create_database_snapshot.sql
@@ -2,12 +2,14 @@ DECLARE
 	 @CurrDB		SYSNAME = DB_NAME()
 	,@WhatIf		BIT = 1
 
-DECLARE @CMD NVARCHAR(MAX);
+DECLARE @CMD NVARCHAR(MAX), @SnapshotName SYSNAME;
+
+SET @SnapshotName = @CurrDB + '_snapshot_' + CONVERT(nvarchar, GETDATE(), 112) + REPLACE(CONVERT(nvarchar, GETDATE(), 114),':','');
 
 SELECT @CMD = ISNULL(@CMD + N',
 ', N'') + N'(NAME = ' + QUOTENAME(name) + N'
 , FILENAME = ' + QUOTENAME(LEFT(physical_name, LEN(physical_name) - CHARINDEX('\', REVERSE(physical_name)) + 1)
-+ DB_NAME(database_id) + '_' + REPLACE(NEWID(),'-','') + '.ss', N'''')
++ @SnapshotName + N'_' + name + '.ss', N'''')
 + N')'
 FROM sys.master_files
 WHERE type <> 1
@@ -21,7 +23,7 @@ ON ' + @CMD, N'')
 AS SNAPSHOT OF ' + QUOTENAME(@CurrDB)
 , @CurrDB = SnapshotName
 FROM
-(VALUES (@CurrDB + '_snapshot_' + CONVERT(nvarchar, GETDATE(), 112) + REPLACE(CONVERT(nvarchar, GETDATE(), 114),':',''))) AS v(SnapshotName)
+(VALUES (@SnapshotName)) AS v(SnapshotName)
 
 PRINT @CMD
 IF @WhatIf = 0 EXEC(@CMD)
