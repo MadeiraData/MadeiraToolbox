a5b5b636ec59b95e3abb81a38125d72d9a709eef
Eitan Blumin <eitan@madeiradata.com>
2021-02-21 12:36:11 +0200

    Specified specific modules

--- Best Practices Checklists/Azure_Find_Unattached_Resources.ps1
+++ Best Practices Checklists/Azure_Find_Unattached_Resources.ps1
@@ -5,38 +5,40 @@
 )
 
 # install AZ modules
-$module = "Az"
-if (Get-Module -ListAvailable -Name $module) {
-    Write-Verbose "$module already installed"
-} 
-else {
-    Write-Information "Installing $module"
-    #Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Confirm:$false
-    Install-Module $module -Force -SkipPublisherCheck -Scope CurrentUser | Out-Null
-    Import-Module $module -Force -PassThru -Scope Local | Out-Null
+$modules = @("Az.Accounts", "Az.Compute", "Az.Storage", "Az.Network")
+        
+foreach ($module in $modules) {
+    if (Get-Module -ListAvailable -Name $module) {
+        Write-Verbose "$module already installed"
+    } 
+    else {
+        Write-Information "Installing $module"
+        Install-Module $module -Force -SkipPublisherCheck -Scope CurrentUser | Out-Null
+        Import-Module $module -Force -PassThru -Scope Local | Out-Null
+    }
 }
 
 # connect to Azure Subscription
- Connect-Azaccount
+Connect-Azaccount
 
- Get-AzSubscription | Where-Object {$_.Name -eq $Subscription} | ForEach-Object {
+Get-AzSubscription | Where-Object {$_.Name -eq $Subscription} | ForEach-Object {
     Write-Output "Switching to subscription '$($_.Name)' in TenantId '$($_.TenantId)'"
     $SubscriptionId = $_.Id
     Select-AzSubscription -TenantId $_.TenantId -SubscriptionId $SubscriptionId | Out-Null
 }
 
 Write-Output "finding unattached managed disks"
- Get-AzDisk | Where-Object {$_.ManagedBy -eq $null} | Select Id
+Get-AzDisk | Where-Object {$_.ManagedBy -eq $null} | Select Id
 
 Write-Output "finding unattached NIC cards"
- Get-AzNetworkInterface | Where-Object {$_.VirtualMachine -eq $null } | Select Id
+Get-AzNetworkInterface | Where-Object {$_.VirtualMachine -eq $null } | Select Id
 
 Write-Output "finding unattached public-ips"
- Get-AzPublicIpAddress | Where-Object {$_.IpConfiguration -eq $null } | Select Id
+Get-AzPublicIpAddress | Where-Object {$_.IpConfiguration -eq $null } | Select Id
 
 Write-Output "finding unattached unmanaged disks"
- $storageAccounts = Get-AzStorageAccount
- foreach($storageAccount in $storageAccounts){
+$storageAccounts = Get-AzStorageAccount
+foreach($storageAccount in $storageAccounts){
      $storageKey = (Get-AzStorageAccountKey -ResourceGroupName $storageAccount.ResourceGroupName -Name $storageAccount.StorageAccountName)[0].Value
      $context = New-AzStorageContext -StorageAccountName $storageAccount.StorageAccountName -StorageAccountKey $storageKey
      $containers = Get-AzStorageContainer -Context $context
