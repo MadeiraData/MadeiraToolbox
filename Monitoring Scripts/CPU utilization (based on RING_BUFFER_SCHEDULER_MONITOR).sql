4316ba1404a124eb4711521dfd5aceca523c2099
Eitan Blumin <eitan@madeiradata.com>
2021-03-07 13:52:22 +0200

    simplified script and separated data into different columns

--- Monitoring Scripts/CPU utilization (based on RING_BUFFER_SCHEDULER_MONITOR).sql
+++ Monitoring Scripts/CPU utilization (based on RING_BUFFER_SCHEDULER_MONITOR).sql
@@ -1,17 +1,23 @@
-DECLARE @TimeStamp bigint, @NumOfSamplesToCheck int
-SET @NumOfSamplesToCheck = 10
+SET NOCOUNT ON;
+DECLARE @NumOfSamplesToCheck int = 10
+
+DECLARE @TimeStamp bigint
 SELECT @TimeStamp = cpu_ticks / (cpu_ticks/ms_ticks) FROM sys.dm_os_sys_info
 
-select top (@NumOfSamplesToCheck) CONVERT(varchar,DATEADD (ms, -1 * (@TimeStamp - [timestamp]), GETDATE()),8) + ' | SQL(' + Cast(SQLServerCPUUtilization as varchar(5)) + '%) | Other(' + Cast((100 - SystemIdle - SQLServerCPUUtilization) as varchar(30)) + '%)',  100 - SystemIdle as TotalCPU
-FROM ( 
-select record.value('(./Record/@id)[1]', 'int') as id,
-record.value('(./Record/SchedulerMonitorEvent/SystemHealth/SystemIdle)[1]', 'int') as SystemIdle,
-record.value('(./Record/SchedulerMonitorEvent/SystemHealth/ProcessUtilization)[1]', 'int') as SQLServerCPUUtilization,
-timestamp
-from (
-select timestamp, convert(xml, record) as record
-from sys.dm_os_ring_buffers
-where ring_buffer_type = N'RING_BUFFER_SCHEDULER_MONITOR'
-and record like '%<SystemHealth>%') as RingBufferInfo
-) AS TabularInfo
-order by id desc
+SELECT
+	DATEADD (ms, -1 * (@TimeStamp - [timestamp]), GETDATE()) AS [SystemTime],
+	countervalues.SystemIdle,
+	countervalues.SQLServerCPUUtilization,
+	100 - SystemIdle - SQLServerCPUUtilization AS OtherCPUUtilization
+FROM (
+	SELECT TOP (@NumOfSamplesToCheck) [timestamp], convert(xml, record) as record
+	FROM sys.dm_os_ring_buffers
+	WHERE ring_buffer_type = N'RING_BUFFER_SCHEDULER_MONITOR'
+	AND record LIKE '%<SystemHealth>%'
+	ORDER BY [timestamp] DESC
+) AS RingBufferInfo
+CROSS APPLY
+(SELECT
+	SystemIdle = record.value('(./Record/SchedulerMonitorEvent/SystemHealth/SystemIdle)[1]', 'int'),
+        SQLServerCPUUtilization = record.value('(./Record/SchedulerMonitorEvent/SystemHealth/ProcessUtilization)[1]', 'int')
+) AS countervalues
