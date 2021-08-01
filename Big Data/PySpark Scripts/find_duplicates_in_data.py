fab41e5ede28a9345a5756bfe3a6384b5cec1a26
mariasql <masania@gmail.com>
2021-08-01 12:45:47 +0300

    add PySpark script that searches for dublicates in data

---
+++ Big Data/PySpark Scripts/find_duplicates_in_data.py
@@ -0,0 +1,17 @@
+# This script will load the parquet files from folder on hdfs or s3 or any other path that is accessable
+# Files can also be of csv or json format, then need to change format parameter
+
+s3_path = 's3://bucket/folder_holding_the_files'
+
+data = spark.read.option("recursiveFileLookup", "true").load(s3_path),format="parquet")
+
+duplicates = data \
+    .groupby(data.columns) \
+    .count() \
+    .where('count > 1')
+
+if duplicates.count() > 0:
+  display(duplicates.sort('count', ascending=False) )
+else:
+  print ("There were no duplicates found in data")
+
