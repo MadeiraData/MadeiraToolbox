6fa31ccea24a0dcdbd4e0784cfde5d01fda32219
Eitan Blumin <eitan@madeiradata.com>
2021-08-12 12:07:56 +0300

    Create Security_Audit_Default_Trace.sql

---
+++ Utility Scripts/Security_Audit_Default_Trace.sql
@@ -0,0 +1,29 @@
+/*
+Get Security Audit trace data from the SQL Server Default Trace
+===============================================================
+This script is based on a sample script provided here:
+https://www.red-gate.com/simple-talk/databases/sql-server/performance-sql-server/the-default-trace-in-sql-server-the-power-of-performance-and-security-auditing/
+*/
+SELECT TOP (1000)
+	te.[name] AS [EventName] ,
+	v.subclass_name ,
+        t.DatabaseName ,
+        t.DatabaseID ,
+        t.NTDomainName ,
+        t.ApplicationName ,
+        t.LoginName ,
+        t.SPID ,
+        t.StartTime ,
+        t.RoleName ,
+        t.TargetUserName ,
+        t.TargetLoginName ,
+        t.SessionLoginName ,
+	t.TextData
+FROM    sys.fn_trace_gettable(CONVERT(VARCHAR(1000), (SELECT TOP (1) [path] FROM sys.traces WHERE is_default = 1)), default) AS t
+        JOIN sys.trace_events te ON t.EventClass = te.trace_event_id
+        JOIN sys.trace_subclass_values v ON v.trace_event_id = te.trace_event_id AND v.subclass_value = t.EventSubClass
+WHERE te.category_id = 8 -- Security Audit
+--WHERE   te.[name] IN ( 'Audit Addlogin Event', 'Audit Add DB User Event',
+--                     'Audit Add Member to DB Role Event', 'Audit Add Login to Server Role Event' )
+--        AND v.subclass_name IN ( 'add', 'Grant database access' )
+ORDER BY t.StartTime DESC
