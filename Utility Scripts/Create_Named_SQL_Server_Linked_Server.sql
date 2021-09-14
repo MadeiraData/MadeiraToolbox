f933d9abd8982065f05064025a002e26dd217eb3
Eitan Blumin <eitan@madeiradata.com>
2021-09-14 13:30:32 +0300

    added Data Source mode as default

--- Utility Scripts/Create_Named_SQL_Server_Linked_Server.sql
+++ Utility Scripts/Create_Named_SQL_Server_Linked_Server.sql
@@ -7,18 +7,26 @@ DECLARE
 	@NewServerName 		[nvarchar](255) = 'MyRemoteServerName',
 	@RemoteUser 		[nvarchar](128) = 'remote_user', -- login name of remote mapped user. If NULL, will not create a mapped login.
 	@RemotePassword		[nvarchar](128) = 'remote_user_password',
-	@MapLocalLogin 		[nvarchar](255) = NULL -- name a local login to map to the remote login. If NULL, will map current login.
+	@MapLocalLogin 		[nvarchar](255) = NULL, -- name a local login to map to the remote login. If NULL, will map current login.
+	@UseProviderString	[bit] = 0		-- set to 1 to use provider string mode, otherwise use data source mode
 
 SET @MapLocalLogin = ISNULL(@MapLocalLogin, SUSER_NAME())
-
-DECLARE @ProviderString NVARCHAR(100);
-SET @ProviderString = N'PROVIDER=SQLNCLI;SERVER=tcp:' + @ServerAddress
 
 -- If linked server already exists, drop it first
 IF EXISTS (SELECT srv.name FROM sys.servers srv WHERE srv.server_id != 0 AND srv.name = @NewServerName)
 	EXEC master.dbo.sp_dropserver @server=@NewServerName, @droplogins='droplogins'
 
-EXEC master.dbo.sp_addlinkedserver @server = @NewServerName, @provider = N'SQLNCLI', @srvproduct=N'MSSQL', @provstr=@ProviderString;
+IF @UseProviderString = 1
+BEGIN
+	DECLARE @ProviderString NVARCHAR(100);
+	SET @ProviderString = N'PROVIDER=SQLNCLI;SERVER=tcp:' + @ServerAddress
+
+	EXEC master.dbo.sp_addlinkedserver @server = @NewServerName, @provider=N'SQLNCLI',@srvproduct=N'MSSQL', @provstr=@ProviderString;
+END
+ELSE
+BEGIN
+	EXEC master.dbo.sp_addlinkedserver @server = @NewServerName, @provider=N'SQLNCLI',@srvproduct='MS SQL Server', @datasrc=@ServerAddress 
+END
 
 EXEC master.dbo.sp_serveroption @server=@NewServerName, @optname=N'collation compatible', @optvalue=N'true'
 EXEC master.dbo.sp_serveroption @server=@NewServerName, @optname=N'data access', @optvalue=N'true'
