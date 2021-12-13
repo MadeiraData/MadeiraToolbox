5f7b7af03e7527f786675cc36ab27e122a8b07bd
Eitan Blumin <eitan@madeiradata.com>
2021-12-13 16:41:45 +0200

    Simplified the script a bit more

--- Utility Scripts/Hypothetical Indexes - Example Usage.sql
+++ Utility Scripts/Hypothetical Indexes - Example Usage.sql
@@ -1,6 +1,5 @@
 
-
-/* TODO: Replace [dbo].[MyTableName] with the name of your specific table */
+/*** TODO: Replace [dbo].[MyTableName] with the name of your specific table ***/
 
 
 -- step 1: CREATE
@@ -8,27 +7,24 @@
 
 /* TODO: Replace with your own index definitions, but don't forget to use WITH STATISTICS_ONLY */
 
-CREATE NONCLUSTERED INDEX [IX_Hypothetical_1]
+CREATE NONCLUSTERED INDEX [IX_Hypothetical]
 ON [dbo].[MyTableName] ( [column1], [column2] )
 WITH STATISTICS_ONLY
-GO
-CREATE NONCLUSTERED INDEX IX_Hypothetical_2
-ON [dbo].[MyTableName] ( [column2], [column3])
-WITH STATISTICS_ONLY
+
 GO
 
 -- step 2: AUTOPILOT
--- Generate and run DBCC AUTOPILOT commands to mark hypothetical indexes for AUTOPILOT:
+-- Generate and run DBCC AUTOPILOT commands to mark ALL hypothetical indexes for AUTOPILOT:
 
 DECLARE @TableName nvarchar(256) = '[dbo].[MyTableName]'
 
--- DBCC AUTOPILOT (TYPE_ID,DB_ID,OBJECT_ID,INDEX_ID);
+-- DBCC AUTOPILOT (typeid [, dbid [, {maxQueryCost | tabid [, indid [, pages [, flag [, rowcounts]]]]} ]])
 DECLARE @cmd nvarchar(max)
 
 DECLARE cmd CURSOR
 LOCAL FAST_FORWARD
 FOR
-SELECT CONCAT(N'DBCC AUTOPILOT(0,', DB_ID(), N',', object_id, N',', index_id, N');')
+SELECT N'DBCC AUTOPILOT(0,' + CONVERT(nvarchar(MAX), DB_ID()) +  N',' + CONVERT(nvarchar(MAX), object_id) + N',' + CONVERT(nvarchar(MAX), index_id) +  N');'
 FROM sys.indexes
 WHERE is_hypothetical = 1
 AND object_id = OBJECT_ID(@TableName)
@@ -48,19 +44,21 @@ CLOSE cmd
 DEALLOCATE cmd
 GO
 
--- step 3: PLAN
+-- step 3: GENERATE ESTIMATED PLAN
 -- run the below to generate an estimated plan assuming the existence of hypothetical indexes marked for autopilot
 -- WARNING: This was found to sometimes cause SQL Crash Dumps, specifically when cancelling mid-execution.
 GO
 SET AUTOPILOT ON;
 GO
+
 /* TODO: Add your test query here */
+
 GO
 SET AUTOPILOT OFF;
 GO
 
 -- step 4: CLEANUP
--- Generate and run DROP commands for all hypothetical indexes on the relevant table:
+-- Generate and run DROP commands for ALL hypothetical indexes on the relevant table:
 
 DECLARE @TableName nvarchar(256) = '[dbo].[MyTableName]'
 
