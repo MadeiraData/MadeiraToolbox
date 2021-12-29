7d2af3e1df4d7f1a8fc7f3577036b42c4b7d5737
Eitan Blumin <eitan@madeiradata.com>
2021-12-29 18:40:11 +0200

    added more passwords, backward compatibility, and support for Azure SQL

--- Health Check Scripts/Check_Passwords_SQL_Server_Generated.sql
+++ Health Check Scripts/Check_Passwords_SQL_Server_Generated.sql
@@ -1,17 +1,609 @@
-DECLARE @OutputPassword bit = 0;
+/*
+Check SQL Logins for Weak Passwords
+===================================
+Author: Eitan Blumin
+Date: 2020-09-02
+Weak passwords list is based on: https://github.com/danielmiessler/SecLists/tree/master/Passwords
+*/
+DECLARE
+	@BringThePain		bit = 0
+,	@OutputPasswords	bit = 0
 
 SET NOCOUNT ON;
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 
+IF OBJECT_ID('tempdb..#pwd') IS NOT NULL DROP TABLE #pwd;
+CREATE TABLE #pwd
+(
+generatedPwd nvarchar(4000) COLLATE Latin1_General_BIN NOT NULL
+);
+
 IF OBJECT_ID('tempdb..#logins') IS NOT NULL DROP TABLE #logins;
 CREATE TABLE #logins
 (
-Deviation nvarchar(4000),
+Deviation varchar(256),
+LoginSID varbinary(4000) NULL,
+LoginPrincipleId int NULL,
 LoginName sysname,
-ServerRoles nvarchar(MAX) NULL,
-ServerPermissions nvarchar(MAX) NULL,
-DBAccess nvarchar(MAX) NULL
+ServerRoles varchar(8000) NULL,
+ServerPermissions varchar(8000) NULL,
+DBAccess varchar(MAX) NULL
 )
+
+IF OBJECT_ID('sys.sql_logins') IS NULL
+BEGIN
+	PRINT N'This script is not supported on this SQL Server edition.';
+	SET NOEXEC ON;
+END
+
+-- Common passwords
+INSERT INTO #pwd WITH (TABLOCKX)
+SELECT [value]
+FROM (VALUES
+ ('1234321')
+,('qweewq')
+,('101010')
+,('10203')
+,('123123123')
+,('147258369')
+,('1qaz2wsx')
+,('789456123')
+,('jordan23')
+,('#SAPassword!')
+,('$easyWinArt4')
+,('$ei$micMicro')
+,('*ARIS!1dm9n#')
+,('123698745')
+,('134679')
+,('142536')
+,('1q2w3e')
+,('1q2w3e4r')
+,('1q2w3e4r5t')
+,('20100728')
+,('25251325')
+,('42Emerson42Eme')
+,('5201314')
+,('686584')
+,('a1b2c3d4')
+,('a801016')
+,('aaron431')
+,('admin')
+,('Administrator1')
+,('AIMS')
+,('alexander')
+,('amanda')
+,('andrea')
+,('andrew')
+,('angel1')
+,('anhyeuem')
+,('anthony')
+,('ashley')
+,('azerty')
+,('bailey')
+,('Bangbang123')
+,('baseball')
+,('basketball')
+,('batman')
+,('blackcoffee333')
+,('blink182')
+,('BPMS')
+,('buster')
+,('butterfly')
+,('capassword')
+,('Cardio.Perfect')
+,('charlie')
+,('chatbooks')
+,('cheese')
+,('chocolate')
+,('cic')
+,('cic!23456789')
+,('computer')
+,('cookie')
+,('daniel')
+,('DBA!sa@EMSDB123')
+,('default')
+,('Dr8gedog')
+,('dragon')
+,('evite')
+,('family')
+,('flower')
+,('gabriel')
+,('ginger')
+,('gnos')
+,('hannah')
+,('hunter')
+,('i2b2demodata')
+,('i2b2demodata2')
+,('i2b2hive')
+,('i2b2metadata')
+,('i2b2metadata2')
+,('i2b2workdata')
+,('i2b2workdata2')
+,('israel')
+,('jacket025')
+,('jakcgt333')
+,('jennifer')
+,('jessica')
+,('jesus1')
+,('jinjer')
+,('jobandtalent')
+,('joshua')
+,('justin')
+,('killer')
+,('letmein')
+,('lovely')
+,('loveme')
+,('M3d!aP0rtal')
+,('madison')
+,('maggieown')
+,('master')
+,('matthew')
+,('maxadmin')
+,('maxreg')
+,('medocheck123')
+,('michelle')
+,('Million2')
+,('monkey')
+,('MULTIMEDIA')
+,('mxintadm')
+,('myspace1')
+,('naruto')
+,('netxms')
+,('nicole')
+,('ohmnamah23')
+,('omgpop')
+,('opengts')
+,('P@$$w0rd')
+,('P@ssw0rd')
+,('Pa$$w0rd')
+,('party')
+,('Pass@123')
+,('passw0rd')
+,('peanut')
+,('pepper')
+,('picture1')
+,('pokemon')
+,('PracticeUser1')
+,('purple')
+,('pwAdmin')
+,('pwddbo')
+,('pwPower')
+,('pwUser')
+,('q1w2e3')
+,('q1w2e3r4')
+,('robert')
+,('RPSsql12345')
+,('samantha')
+,('Sample123')
+,('samsung')
+,('SECAdmin1')
+,('SecurityMaster08')
+,('senha')
+,('sha256')
+,('shadow')
+,('SilkCentral12!34')
+,('skf_admin1')
+,('soccer')
+,('splendidcrm2005')
+,('sqlserver')
+,('starwars')
+,('stream-1')
+,('summer')
+,('sunshine')
+,('superadmin')
+,('superman')
+,('taylor')
+,('thomas')
+,('tigger')
+,('trinity')
+,('trustno1')
+,('unknown')
+,('V4in$ight')
+,('vampire')
+,('vantage12!')
+,('wasadmin')
+,('welcome')
+,('whatever')
+,('wwAdmin')
+,('wwPower')
+,('wwUser')
+,('x4ivygA51F')
+,('yugioh')
+,('zing')
+,('4128')
+,('5150')
+,('8675309')
+,('abgrtyu')
+,('access')
+,('access14')
+,('action')
+,('albert')
+,('alex')
+,('alexis')
+,('amateur')
+,('angel')
+,('angela')
+,('angels')
+,('animal')
+,('apollo')
+,('apple')
+,('apples')
+,('arsenal')
+,('arthur')
+,('asshole')
+,('august')
+,('austin')
+,('baby')
+,('badboy')
+,('banana')
+,('barney')
+,('beach')
+,('bear')
+,('beaver')
+,('beavis')
+,('beer')
+,('bigcock')
+,('bigdaddy')
+,('bigdick')
+,('bigdog')
+,('bigtits')
+,('bill')
+,('billy')
+,('birdie')
+,('bitch')
+,('bitches')
+,('biteme')
+,('black')
+,('blazer')
+,('blonde')
+,('blondes')
+,('blowjob')
+,('blowme')
+,('blue')
+,('bond007')
+,('bonnie')
+,('booboo')
+,('boobs')
+,('booger')
+,('boomer')
+,('booty')
+,('boston')
+,('brandon')
+,('brandy')
+,('braves')
+,('brazil')
+,('brian')
+,('bronco')
+,('broncos')
+,('bubba')
+,('buddy')
+,('bulldog')
+,('butter')
+,('butthead')
+,('calvin')
+,('camaro')
+,('cameron')
+,('canada')
+,('captain')
+,('carlos')
+,('carter')
+,('casper')
+,('charles')
+,('chelsea')
+,('chester')
+,('chevy')
+,('chicago')
+,('chicken')
+,('chris')
+,('cocacola')
+,('cock')
+,('coffee')
+,('college')
+,('compaq')
+,('cool')
+,('cooper')
+,('corvette')
+,('cowboy')
+,('cowboys')
+,('cream')
+,('crystal')
+,('cumming')
+,('cumshot')
+,('cunt')
+,('dakota')
+,('dallas')
+,('danielle')
+,('dave')
+,('david')
+,('debbie')
+,('dennis')
+,('diablo')
+,('diamond')
+,('dick')
+,('dirty')
+,('doctor')
+,('doggie')
+,('dolphin')
+,('dolphins')
+,('donald')
+,('dreams')
+,('driver')
+,('eagle')
+,('eagle1')
+,('eagles')
+,('edward')
+,('einstein')
+,('enjoy')
+,('enter')
+,('eric')
+,('erotic')
+,('extreme')
+,('falcon')
+,('fender')
+,('ferrari')
+,('fire')
+,('firebird')
+,('fish')
+,('fishing')
+,('florida')
+,('flyers')
+,('ford')
+,('forever')
+,('frank')
+,('fred')
+,('freddy')
+,('freedom')
+,('fuck')
+,('fucked')
+,('fucker')
+,('fucking')
+,('fuckme')
+,('gandalf')
+,('gateway')
+,('gators')
+,('gemini')
+,('george')
+,('giants')
+,('girl')
+,('girls')
+,('golden')
+,('golf')
+,('golfer')
+,('gordon')
+,('great')
+,('green')
+,('gregory')
+,('guitar')
+,('gunner')
+,('hammer')
+,('happy')
+,('hardcore')
+,('harley')
+,('heather')
+,('helpme')
+,('hentai')
+,('hockey')
+,('hooters')
+,('horney')
+,('horny')
+,('hotdog')
+,('house')
+,('hunting')
+,('iceman')
+,('internet')
+,('iwantu')
+,('jack')
+,('jackie')
+,('jackson')
+,('jaguar')
+,('jake')
+,('james')
+,('japan')
+,('jasmine')
+,('jason')
+,('jasper')
+,('jeremy')
+,('john')
+,('johnny')
+,('johnson')
+,('joseph')
+,('juice')
+,('junior')
+,('kelly')
+,('kevin')
+,('king')
+,('kitty')
+,('knight')
+,('ladies')
+,('lakers')
+,('lauren')
+,('leather')
+,('legend')
+,('little')
+,('london')
+,('lover')
+,('lovers')
+,('lucky')
+,('maddog')
+,('maggie')
+,('magic')
+,('magnum')
+,('marine')
+,('mark')
+,('marlboro')
+,('martin')
+,('marvin')
+,('matrix')
+,('matt')
+,('maverick')
+,('maxwell')
+,('melissa')
+,('member')
+,('mercedes')
+,('merlin')
+,('mickey')
+,('midnight')
+,('mike')
+,('miller')
+,('mine')
+,('mistress')
+,('money')
+,('monica')
+,('monster')
+,('morgan')
+,('mother')
+,('mountain')
+,('movie')
+,('muffin')
+,('murphy')
+,('music')
+,('mustang')
+,('naked')
+,('nascar')
+,('nathan')
+,('naughty')
+,('ncc1701')
+,('newyork')
+,('nicholas')
+,('nipple')
+,('nipples')
+,('oliver')
+,('orange')
+,('ou812')
+,('packers')
+,('panther')
+,('panties')
+,('paris')
+,('parker')
+,('pass')
+,('patrick')
+,('paul')
+,('peaches')
+,('penis')
+,('peter')
+,('phantom')
+,('phoenix')
+,('player')
+,('please')
+,('pookie')
+,('porn')
+,('porno')
+,('porsche')
+,('power')
+,('prince')
+,('private')
+,('pussies')
+,('pussy')
+,('rabbit')
+,('rachel')
+,('racing')
+,('raiders')
+,('rainbow')
+,('ranger')
+,('rangers')
+,('rebecca')
+,('redskins')
+,('redsox')
+,('redwings')
+,('richard')
+,('rock')
+,('rocket')
+,('rosebud')
+,('runner')
+,('rush2112')
+,('russia')
+,('sammy')
+,('samson')
+,('sandra')
+,('saturn')
+,('scooby')
+,('scooter')
+,('scorpio')
+,('scorpion')
+,('scott')
+,('secret')
+,('sexsex')
+,('sexy')
+,('shannon')
+,('shaved')
+,('shit')
+,('sierra')
+,('silver')
+,('skippy')
+,('slayer')
+,('slut')
+,('smith')
+,('smokey')
+,('snoopy')
+,('sophie')
+,('spanky')
+,('sparky')
+,('spider')
+,('squirt')
+,('srinivas')
+,('star')
+,('stars')
+,('startrek')
+,('steelers')
+,('steve')
+,('steven')
+,('sticky')
+,('stupid')
+,('success')
+,('suckit')
+,('super')
+,('surfer')
+,('swimming')
+,('sydney')
+,('teens')
+,('tennis')
+,('teresa')
+,('test')
+,('tester')
+,('testing')
+,('theman')
+,('thunder')
+,('thx1138')
+,('tiffany')
+,('tiger')
+,('tigers')
+,('time')
+,('tits')
+,('tomcat')
+,('topgun')
+,('toyota')
+,('travis')
+,('trouble')
+,('tucker')
+,('turtle')
+,('united')
+,('vagina')
+,('victor')
+,('victoria')
+,('video')
+,('viking')
+,('viper')
+,('voodoo')
+,('voyager')
+,('walter')
+,('warrior')
+,('white')
+,('william')
+,('willie')
+,('wilson')
+,('winner')
+,('winston')
+,('winter')
+,('wizard')
+,('wolf')
+,('women')
+,('xavier')
+,('yamaha')
+,('yankee')
+,('yankees')
+,('yellow')
+,('young')) AS v([value])
+OPTION (RECOMPILE); -- avoid saving this in plan cache
 
 ;WITH NumbersCTE
 AS
@@ -47,9 +639,13 @@ AS
 (
 	SELECT V.TxtWord
 	FROM (VALUES
-	('123'),('1234'),('12345'),('456'),('654'),
-	('asd'),('qwe'),('qwer'),('qwert'),('qwerty'),('asdf'),('zxc'),('abc'),('abcd'),
-	('159'),('951'),('753'),('357'),('147'),('741'),('258'),('852'),('369'),('963')
+	('12'),('21'),('123'),('1234'),('12345'),('123456'),('1234567'),('12345678'),('123456789'),('456'),('789'),('987'),('654'), --('789456'),
+	('asd'),('qwe'),('qwer'),('qwert'),('qwerty'),('asdf'),('zxc'),('vbn'),('abc'),('abcd'),('uiop'),('zxcvbnm'),
+	('1qaz'),('qaz'),('zaq'),('wsx'),('xsw'),('12qw'),('aszx'),('321'),('Aa'),('asdqwe'),('hello'),('qqww'),('1122'),
+	('159'),('951'),('753'),('357'),('147'),('741'),('258'),('852'),('369'),('963'),('6655'),
+	('a'),('b'),('0'),('1'),('2'),('3'),('!'),('#'),
+	('10'),('20'),('00'),('fgh'),('jkl'),('asdfghjkl'),('ui'),('lol'),('love'),
+	('fuckyou'),('iloveu'),('iloveyou'),('password'),('babygirl'),('football'),('jordan'),('michael'),('princess')
 	) AS V(TxtWord)
 ), CTE_Level2
 AS
@@ -57,8 +653,6 @@ AS
 SELECT CTE.generatedPwd
 FROM NumbersCTE AS CTE
 WHERE CTE.currLength = CTE.Pwdmaxlength
-UNION ALL
-SELECT '1234567890'
 ), GeneratedPasswords
 AS
 (
@@ -72,126 +666,114 @@ UNION ALL
 SELECT REVERSE(CTE_Level2.generatedPwd)
 FROM CTE_Level2
 
-UNION
+UNION ALL
 
 -- Replicated text
 SELECT REPLICATE(V.TxtChar, Occ.Occurences)
 FROM (VALUES
 ('0',4,10),('1',4,10),('2',4,10),('3',4,10),('4',4,10),('5',4,10),('6',4,10),('7',4,10),('8',4,10),('9',4,10),('a',4,10)
-,('123',1,4),('abc',1,4),('a1',1,4),('qwe',1,4),('12',1,4),('21',1,4),('13',1,4),('159',1,3),('456',1,3),('23',1,3),('1234',2,3),('147',1,3),('321',1,3)
-,('bla',1,4),('25',2,4),('69',2,4),('100',2,4),('qwerty',1,2),('x',3,8),('asd',1,3)
+,('123',4,4),('abc',4,4),('a1',1,4),('qwe',4,4),('12',1,4),('21',1,4),('13',1,4),('23',1,3)
+,('bla',1,4),('25',2,4),('69',2,4),('100',2,4),('x',3,8),('z',1,10)
 ) AS V(TxtChar,MinOccurences,MaxOccurences)
 INNER JOIN
 (SELECT TOP (10) ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS Occurences FROM sys.all_columns) AS Occ
 ON Occ.Occurences BETWEEN V.MinOccurences AND V.MaxOccurences
 
-UNION
+UNION ALL
 
--- Duplicate and reverse
-SELECT V.T + ISNULL(V.Mid,'') + REVERSE(V.T)
-FROM (VALUES
-('123','4'),
-('123',''),
-('159',''),
-('qwe','')
-) AS V(T,Mid)
+-- Common permutations (singles)
+SELECT TxtWord
+FROM CommonPermutationWords
 
-UNION
+UNION ALL
 
--- Common permutations
+-- Common permutations (doubles)
 SELECT a.TxtWord + b.TxtWord
 FROM CommonPermutationWords AS a
 CROSS JOIN CommonPermutationWords AS b
 
-UNION
+UNION ALL
 
--- Common words
-SELECT V.TxtWord
-FROM (VALUES
- ('password')
-,('password1')
-,('P@ssw0rd')
-,('passw0rd')
-,('P@$$w0rd')
-,('Pa$$w0rd')
-,('passw0rd')
-,('Aa123456')
-,('1q2w3e4r')
-,('sha256')
-,('1q2w3e')
-,('1qaz2wsx')
-,('q1w2e3r4')
-,('q1w2e3')
-,('1q2w3e4r5t')
-,('a123456'),('123456a'),('a1234567'),('a12345')
-,('blackcoffee333')
-,('qazwsx')
-,('letmein')
-,('israel')
-,('iloveyou')
-,('1qazxsw2')
-,('142536')
-,('147258369')
-,('134679')
-,('dragon')
-,('asdqwe123')
-,('12qwaszx')
-,('123698745')
-,('123654789')
-,('zaq12wsx')
-,('a1b2c3d4')
-,('aaron431')
-,('qqww1122')
-,('monkey')
-,('ginger')
-,('jinjer')
-,('vampire')
-) AS V(TxtWord)
+-- Common permutations (triples)
+SELECT a.TxtWord + b.TxtWord + c.TxtWord
+FROM CommonPermutationWords AS a
+CROSS JOIN CommonPermutationWords AS b
+CROSS JOIN CommonPermutationWords AS c
+WHERE @BringThePain = 1
 )
-INSERT INTO #logins
-(Deviation, LoginName, ServerRoles, ServerPermissions)
+-- Generated passwords
+INSERT INTO #pwd
+SELECT LTRIM(RTRIM(generatedPwd))
+FROM GeneratedPasswords
+OPTION (RECOMPILE); -- avoid saving this in plan cache
+
+IF @BringThePain = 1
+BEGIN
+	IF CONVERT(int, SERVERPROPERTY('EngineEdition')) NOT IN (1,2,4)
+	OR CONVERT(int, (@@microsoftversion / 0x1000000) & 0xff) >= 13
+	CREATE CLUSTERED INDEX IXC ON #pwd (generatedPwd) WITH(DATA_COMPRESSION = PAGE);
+	ELSE
+	CREATE CLUSTERED INDEX IX ON #pwd (generatedPwd);
+END
+
+INSERT INTO #logins WITH (TABLOCKX)
+(Deviation, LoginSID, LoginPrincipleId, LoginName, ServerRoles)
 SELECT 
-Deviation = dev.Deviation + CASE WHEN @OutputPassword = 1 THEN N' (' + Pwd + N')' ELSE N'' END
+Deviation = dev.Deviation + CASE WHEN @OutputPasswords = 1 THEN N' (' + Pwd + N')' ELSE N'' END
+, dev.[sid]
+, dev.principal_id
 , [LoginName]
 , ServerRoles =
 STUFF((
 	SELECT N', ' + roles.name
 	FROM sys.server_role_members AS srm
 	INNER JOIN sys.server_principals AS roles ON srm.role_principal_id = roles.principal_id
-	WHERE srm.member_principal_id = SUSER_ID(dev.[LoginName])
-	FOR XML PATH('')
-	), 1, 2, N'')
-, ServerPermissions =
-STUFF((
-	SELECT N', ' + perm.state_desc + N' ' + perm.permission_name + N' ' + perm.class_desc
-	FROM sys.server_permissions AS perm
-	WHERE perm.grantee_principal_id = SUSER_ID([LoginName])
+	WHERE srm.member_principal_id = dev.principal_id
 	FOR XML PATH('')
 	), 1, 2, N'')
 FROM
 (
-SELECT 'Empty Password' AS Deviation, RTRIM(name) AS [LoginName], '' AS Pwd
-FROM master.sys.sql_logins
-WHERE ([password_hash] IS NULL OR PWDCOMPARE('', [password_hash]) = 1)
+SELECT 'Empty Password' AS Deviation, s.sid, s.principal_id, RTRIM(name) AS [LoginName], '' AS Pwd
+FROM sys.sql_logins AS s
+WHERE is_disabled = 0
+AND ([password_hash] IS NULL OR PWDCOMPARE('', [password_hash]) = 1)
 AND name NOT IN ('MSCRMSqlClrLogin')
-AND name NOT LIKE '##MS_%##'
-AND is_disabled = 0
+AND name NOT LIKE '##MS[_]%##'
 
 UNION ALL
 
-SELECT DISTINCT 'Login Name is the same as Password' AS Deviation, RTRIM(s.name) AS [Name] , RTRIM(RTRIM(s.name)) AS Pwd
-FROM master.sys.sql_logins s 
-WHERE PWDCOMPARE(RTRIM(RTRIM(s.name)), s.[password_hash]) = 1
-AND s.is_disabled = 0
+SELECT DISTINCT 'Login name is the same as password' AS Deviation, s.sid, s.principal_id, RTRIM(s.name) AS [Name] , u.usrname
+FROM sys.sql_logins s
+CROSS APPLY
+(VALUES
+(RTRIM(RTRIM(s.name))),
+(REVERSE(RTRIM(RTRIM(s.name))))
+) AS u(usrname)
+WHERE s.is_disabled = 0
+AND PWDCOMPARE(u.usrname, s.[password_hash]) = 1
 
 UNION ALL
 
-SELECT DISTINCT N'Weak Password' AS Deviation, RTRIM(s.name) AS [LoginName], RTRIM(RTRIM(d.generatedPwd)) AS Pwd
-FROM GeneratedPasswords d
-INNER JOIN master.sys.sql_logins s ON PWDCOMPARE(RTRIM(RTRIM(d.generatedPwd)), s.[password_hash]) = 1
+SELECT DISTINCT 'Weak Password' AS Deviation, s.sid, s.principal_id, RTRIM(s.name) AS [LoginName], d.generatedPwd
+FROM #pwd d
+INNER JOIN sys.sql_logins s ON PWDCOMPARE(d.generatedPwd, s.[password_hash]) = 1
 WHERE s.is_disabled = 0
 ) AS dev
 OPTION (RECOMPILE); -- avoid saving this in plan cache
+
+IF OBJECT_ID('sys.server_permissions') IS NOT NULL
+BEGIN
+	UPDATE dev
+		SET ServerPermissions =
+		STUFF((
+			SELECT N', ' + perm.state_desc + N' ' + perm.permission_name + N' ' + perm.class_desc
+			FROM sys.server_permissions AS perm
+			WHERE perm.grantee_principal_id = dev.LoginPrincipleId
+			FOR XML PATH('')
+			), 1, 2, N'')
+	FROM #logins AS dev
+	OPTION (RECOMPILE);
+END
 
 DECLARE @CurrDB sysname, @Executor nvarchar(1000), @Cmd nvarchar(MAX), @DBAccess nvarchar(MAX);
 
@@ -208,7 +790,7 @@ UPDATE logins
 	), 1, 2, N'''')
 	+ N'')'', N'''')
 FROM #logins AS logins
-INNER JOIN sys.database_principals AS dp ON dp.sid = SUSER_SID(logins.LoginName)'
+INNER JOIN sys.database_principals AS dp ON dp.sid = logins.LoginSID'
 
 DECLARE DBs CURSOR
 FAST_FORWARD LOCAL
@@ -234,4 +816,11 @@ END
 CLOSE DBs;
 DEALLOCATE DBs;
 
-SELECT * FROM #logins;
+SET NOEXEC OFF;
+
+SELECT Deviation
+     , LoginName
+     , ServerRoles
+     , ServerPermissions
+     , DBAccess
+FROM #logins;
