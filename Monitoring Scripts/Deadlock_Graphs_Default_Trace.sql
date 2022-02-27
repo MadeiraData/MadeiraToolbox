6047fc9e08924db0a534c4c628fbd2f80b192e22
Eitan Blumin <eitan@madeiradata.com>
2022-02-27 19:17:28 +0200

    added log file name parsing and TextData filtering

--- Monitoring Scripts/Deadlock_Graphs_Default_Trace.sql
+++ Monitoring Scripts/Deadlock_Graphs_Default_Trace.sql
@@ -1,15 +1,17 @@
-declare @filename nvarchar(200)
+DECLARE @curr_tracefilename varchar(500), @base_tracefilename varchar(500), @indx int ;
+ 
+SELECT @curr_tracefilename = path from sys.traces where is_default = 1 ; 
+SET @curr_tracefilename = reverse(@curr_tracefilename);
+ 
+SELECT @indx  = patindex('%\%', @curr_tracefilename) ;
+SET @curr_tracefilename = reverse(@curr_tracefilename) ;
+ 
+SET @base_tracefilename = left( @curr_tracefilename,len(@curr_tracefilename) - @indx) + '\log.trc' ; 
 
-select TOP 1 @filename = convert(nvarchar(200), value)
-from ::fn_trace_getinfo(null)
-where property = 2
-and [value] IS NOT NULL
---and convert(nvarchar(200), value) LIKE '%deadlocks%'
-ORDER BY traceid ASC
-
-PRINT @filename
+PRINT @base_tracefilename
 
 select StartTime, convert(xml, TextData) AS Deadlock_Graph, ServerName
-from ::fn_trace_gettable(@filename,default)
+from ::fn_trace_gettable(@base_tracefilename,default)
 WHERE TextData IS NOT NULL
+AND TextData LIKE N'%deadlock%'
 order by 1 desc
