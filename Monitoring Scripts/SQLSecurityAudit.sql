c477bd6fe98d4c2788963aafa5c0ce9cabd31b5c
Eitan Blumin <eitan@madeiradata.com>
2022-03-02 18:45:38 +0200

    various fixes

--- Monitoring Scripts/SQLSecurityAudit.sql
+++ Monitoring Scripts/SQLSecurityAudit.sql
@@ -8,7 +8,7 @@ BEGIN
 	TO FILE 
 	(	FILEPATH = N'C:\Temp'
 		,MAXSIZE = 200 MB
-		,MAX_FILES = 100
+		,MAX_ROLLOVER_FILES = 100
 		,RESERVE_DISK_SPACE = OFF
 	)
 	WITH
@@ -23,8 +23,7 @@ IF EXISTS (SELECT * FROM sys.server_audits WHERE name = 'SQLSecurityAudit' AND i
 BEGIN
 	RAISERROR(N'Enabling server audit SQLSecurityAudit',0,1) WITH NOWAIT;
 
-	ALTER SERVER AUDIT [SQLSecurityAudit]  
-	WITH (STATE = ON);  
+	ALTER SERVER AUDIT [SQLSecurityAudit] WITH (STATE = ON);  
 END
 ELSE
 	RAISERROR(N'Server audit SQLSecurityAudit already enabled',0,1) WITH NOWAIT;
@@ -32,7 +31,8 @@ GO
 IF NOT EXISTS (SELECT * FROM sys.server_audit_specifications WHERE name = 'SQLSecurityAuditSpecification')
 BEGIN
 	RAISERROR(N'Creating server audit specification SQLSecurityAuditSpecification',0,1) WITH NOWAIT;
-	CREATE SERVER AUDIT SPECIFICATION [SQLSecurityAuditSpecification] FOR SERVER AUDIT [SQLSecurityAudit]
+	
+	CREATE SERVER AUDIT SPECIFICATION [SQLSecurityAuditSpecification] FOR SERVER AUDIT [SQLSecurityAudit];
 END
 ELSE
 	RAISERROR(N'Server audit specification SQLSecurityAuditSpecification already exists',0,1) WITH NOWAIT;
@@ -43,7 +43,10 @@ SELECT @cmd = ISNULL(@cmd + N',' + CHAR(13), N'') + N' ADD (' + q.covering_paren
 FROM (SELECT DISTINCT covering_parent_action_name
 FROM sys.dm_audit_actions
 WHERE parent_class_desc = 'SERVER'
-AND covering_parent_action_name LIKE '%CHANGE%') AS q
+AND covering_parent_action_name LIKE '%CHANGE%'
+AND covering_parent_action_name NOT IN
+('DATABASE_CHANGE_GROUP','DATABASE_OBJECT_CHANGE_GROUP','SCHEMA_OBJECT_CHANGE_GROUP','SERVER_OBJECT_CHANGE_GROUP')
+) AS q
 WHERE NOT EXISTS
 (
 SELECT TOP (1) NULL
@@ -53,16 +56,14 @@ INNER JOIN sys.server_file_audits AS sfa ON sas.audit_guid = sfa.audit_guid
 WHERE sas.name = 'SQLSecurityAuditSpecification'
 AND q.covering_parent_action_name COLLATE DATABASE_DEFAULT = sasd.audit_action_name COLLATE DATABASE_DEFAULT
 )
-;
+
 IF @@ROWCOUNT > 0
 BEGIN
 	IF EXISTS (SELECT * FROM sys.server_audit_specifications WHERE is_state_enabled = 1 AND name = 'SQLSecurityAuditSpecification')
 	BEGIN
 		RAISERROR(N'Disabling server audit specification SQLSecurityAuditSpecification',0,1) WITH NOWAIT;
 
-		ALTER SERVER AUDIT SPECIFICATION SQLSecurityAuditSpecification  
-		FOR SERVER AUDIT SQLSecurityAudit  
-		WITH (STATE=OFF);  
+		ALTER SERVER AUDIT SPECIFICATION SQLSecurityAuditSpecification WITH (STATE=OFF);  
 	END
 
 	SET @cmd = N'ALTER SERVER AUDIT SPECIFICATION [SQLSecurityAuditSpecification] FOR SERVER AUDIT [SQLSecurityAudit]'
@@ -73,13 +74,10 @@ BEGIN
 	
 	RAISERROR(N'Enabling server audit specification SQLSecurityAuditSpecification',0,1) WITH NOWAIT;
 
-	ALTER SERVER AUDIT SPECIFICATION SQLSecurityAuditSpecification  
-	FOR SERVER AUDIT SQLSecurityAudit  
-	WITH (STATE=ON);  
+	ALTER SERVER AUDIT SPECIFICATION SQLSecurityAuditSpecification WITH (STATE=ON);  
 END
 ELSE
 	RAISERROR(N'Server audit specification SQLSecurityAuditSpecification already fully configured',0,1) WITH NOWAIT;
-
 GO
 SET NOCOUNT ON;
 
@@ -101,6 +99,7 @@ AND daa.covering_parent_action_name COLLATE DATABASE_DEFAULT = sasd.audit_action
 RAISERROR(N'Audit: "%s", File: "%s"',0,1,@AuditName,@File) WITH NOWAIT;
 
 SELECT *
+	--event_time, statement, session_server_principal_name
 	--event_time, statement, session_server_principal_name, host_name, client_ip, application_name
 FROM sys.fn_get_audit_file(@File, default, default)
 --WHERE statement <> N''
