f8bb7a6faac7112824fcef97bc7db9303d7eb415
EricMadeira <eric@madeiradata.com>
2022-03-19 21:01:42 +0200

    Run any T-SQL command for each Azure SQL database

---
+++ Maintenance of Azure SQL Databases/ForEachDB_AzureSQL.ps1
@@ -0,0 +1,20 @@
+﻿#Set Parmeters:
+
+  $ServerInstance = 'yourserver.database.windows.net' # <============ Insert your Server Name
+  $Database = 'master' 
+  $UserName = 'yourusername' # <============ Insert your User Name
+  $Password = 'Yourp@ssword123' # <============ Insert your Password
+  $Query = 'SELECT name FROM sys.databases WHERE database_id > 1'
+  $TargetTenants = Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $Database -UserName $UserName -Password $Password -Query $Query
+
+  $Query = 'CREATE TABLE dbo.TestTable' # <============ Insert your command between the brackets
+
+#Set a foreach loop for each of the Target Tenants:
+
+  $TargetTenants | ForEach-Object{
+  $TargetTenant = $_.name
+  Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $TargetTenant -UserName $UserName -Password $Password -Query $Query -Verbose
+  Write-Host "Executing" $Query "in database" $TargetTenant
+  }
+
+ 
---
+++ Maintenance of Azure SQL Databases/ForEachDB_AzureSQL_File.ps1
@@ -0,0 +1,17 @@
+﻿#Set Parmeters:
+
+  $ServerInstance = 'yourserver.database.windows.net' # <============ Insert your Server Name
+  $Database = 'master' 
+  $UserName = 'yourusername' # <============ Insert your Password
+  $Password = 'Yourp@ssword123' # <============ Insert your User Name
+  $Query = 'SELECT name FROM sys.databases WHERE database_id NOT IN(1,5)'
+  $TargetTenants = Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $Database -UserName $UserName -Password $Password -Query $Query
+
+  $InputFile = '' # <============ Insert your file path between the brackets
+
+#Set a foreach loop for each of the Target Tenants:
+
+  $TargetTenants | ForEach-Object{
+  $TargetTenant = $_.name
+  Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $TargetTenant -UserName $UserName -Password $Password -InputFile $InputFile -Verbose
+  }
