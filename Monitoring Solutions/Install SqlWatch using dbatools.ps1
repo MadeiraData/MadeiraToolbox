1e2085a078d0328620d3e23fb3ad0d2a3da9d093
Eitan Blumin <eitan@madeiradata.com>
2022-04-17 08:48:11 +0300

    Create Install SqlWatch using dbatools.ps1

---
+++ Monitoring Solutions/Install SqlWatch using dbatools.ps1
@@ -0,0 +1,34 @@
+﻿param
+(
+[string[]]$TargetSqlInstances = $env:COMPUTERNAME
+)
+
+
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+if (Get-PSRepository -Name "PSGallery") {
+    Write-Verbose "PSGallery already registered"
+} 
+else {
+    Write-Output "Registering PSGallery..."
+    Register-PSRepository -Default
+}
+
+## you can add or remove additional modules here as needed
+$modules = @("dbatools")
+        
+foreach ($module in $modules) {
+    if (Get-Module -ListAvailable -Name $module) {
+        Write-Output "$module already installed"
+    } 
+    else {
+        Write-Output "Installing $module..."
+        Install-Module $module -Force -SkipPublisherCheck -Scope CurrentUser | Out-Null
+        Write-Output "Importing $module..."
+        Import-Module $module -Force -PassThru -Scope Local | Out-Null
+    }
+}
+
+Write-Output "Installing SqlWatch..."
+Install-DbaSqlWatch -SqlInstance $TargetSqlInstances -Database SQLWATCH
