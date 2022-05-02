b91d5927334dffa4ad45504e400bdd031296ba35
Eitan Blumin <eitan@madeiradata.com>
2022-05-02 17:21:03 +0300

    Create Powershell Get Procedure Parameters using SMO.ps1

---
+++ Utility Scripts/Powershell Get Procedure Parameters using SMO.ps1
@@ -0,0 +1,20 @@
+﻿param
+(
+    [string]$InstanceName = $env:COMPUTERNAME,
+    [string]$DBName = "MyDB",
+    [string]$ProcedureSchema = "dbo",
+    [string]$ProcedureName = "SQLLogAdd"
+)
+
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO') | Out-Null
+
+$serverInstance = New-Object ('Microsoft.SqlServer.Management.Smo.Server') $InstanceName
+
+$serverInstance.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.StoredProcedure], $false) # Limit SMO retrieval to procedures only
+
+$procedure = $serverInstance.Databases[$DBName].StoredProcedures[$ProcedureName, $ProcedureSchema];
+
+$procedure.Parameters | Select-Object Name, DataType, DefaultValue, @{Name="Properties";Expression={$_.Properties | Where Name -in "Length", "NumericPrecision", "NumericScale", "IsOutputParameter" | Select Name, Value }}
+
+
+# $procedure.Parameters[0].Properties | Select Name, Value       # <--- run this to see a list of available parameter properties
