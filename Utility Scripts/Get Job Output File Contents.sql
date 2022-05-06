9341a04eb75064fac5097339967ecb70c9cb23f1
Eitan Blumin <eitan@madeiradata.com>
2022-05-06 11:37:57 +0300

    Update Get Job Output File Contents.sql

--- Utility Scripts/Get Job Output File Contents.sql
+++ Utility Scripts/Get Job Output File Contents.sql
@@ -7,20 +7,22 @@ Date: 2022-04-13
 DECLARE @JobName sysname = N'DatabaseBackup - USER_DATABASES - FULL'
 
 SET NOCOUNT ON;
+DECLARE @cmd nvarchar(MAX), @LogDirectory nvarchar(4000), @OutputFilePath nvarchar(MAX), @jobId varbinary(64), @jobStepId int, @jobStepName sysname
 
-IF NOT EXISTS
-(
-SELECT NULL
+SELECT	  @JobName = j.name
+	, @jobId = CONVERT(varbinary(256), js.job_id)
+	, @jobStepId = js.step_id
+	, @jobStepName = js.step_name
+	, @OutputFilePath = js.output_file_name
 FROM msdb.dbo.sysjobs AS j
 INNER JOIN msdb.dbo.sysjobsteps AS js ON j.job_id = js.job_id
 WHERE j.name = @JobName AND js.output_file_name <> N''
-)
+
+IF @OutputFilePath IS NULL
 BEGIN
 	RAISERROR(N'Job "%s" is not a valid job with output.', 16,1,@JobName);
+	SET NOEXEC ON;
 END
-
-DECLARE @LogDirectory nvarchar(4000), @cmd nvarchar(MAX), @OutputFilePath nvarchar(MAX);
-DECLARE @FilesList table (id int IDENTITY(1,1) NOT NULL, subdir nvarchar(MAX) NULL, depth smallint NOT NULL, isFile tinyint NOT NULL);
 
 IF CONVERT(int, (@@microsoftversion / 0x1000000) & 0xff) >= 11
 BEGIN
@@ -33,21 +35,48 @@ BEGIN
 	SET @LogDirectory = LEFT(CAST(SERVERPROPERTY('ErrorLogFileName') AS nvarchar(max)),LEN(CAST(SERVERPROPERTY('ErrorLogFileName') AS nvarchar(max))) - CHARINDEX('\',REVERSE(CAST(SERVERPROPERTY('ErrorLogFileName') AS nvarchar(max)))))
 END
 
+--SELECT @OutputFilePath
+SET @OutputFilePath =
+	REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
+		@OutputFilePath
+		, N'$(ESCAPE_SQUOTE(SQLLOGDIR))\', @LogDirectory)
+		, N'$(ESCAPE_SQUOTE(JOBNAME))', @JobName)
+		, N'$(ESCAPE_SQUOTE(JOBID))', CONVERT(nvarchar(4000), @jobId, 1))
+		, N'$(ESCAPE_SQUOTE(STEPNAME))', @jobStepName)
+		, N'$(ESCAPE_SQUOTE(STEPID))', @jobStepId)
+		, N'$(ESCAPE_SQUOTE(DATE))', N'%')
+		, N'$(ESCAPE_SQUOTE(TIME))', N'%')
+
 RAISERROR(N'Job Name: %s', 0, 1, @JobName);
 RAISERROR(N'Log Directory: %s', 0, 1, @LogDirectory);
+RAISERROR(N'Output File Path: %s', 0, 1, @OutputFilePath);
 
-INSERT INTO @FilesList (subdir, depth, isFile)
-EXEC sys.xp_dirtree @LogDirectory, 0, 1
+-- If there are wildcards in the path, we'll need to parse the folder contents to find the exact file
+IF CHARINDEX('%', @OutputFilePath) > 0
+BEGIN
+	SET @LogDirectory = LEFT(@OutputFilePath, LEN(@OutputFilePath) - CHARINDEX(N'\', REVERSE(@OutputFilePath)) + 1)
+	SET @OutputFilePath = REPLACE(@OutputFilePath, @LogDirectory, N'')
 
-SELECT TOP (1) @OutputFilePath = @LogDirectory + subdir
-FROM @FilesList
-WHERE subdir LIKE @JobName + N'%[_]%[_]%.txt'
-ORDER BY subdir DESC
+	DECLARE @FilesList table (id int IDENTITY(1,1) NOT NULL, subdir nvarchar(MAX) NULL, depth smallint NOT NULL, isFile tinyint NOT NULL);
+	INSERT INTO @FilesList (subdir, depth, isFile)
+	EXEC sys.xp_dirtree @LogDirectory, 0, 1
 
-SET @cmd = N'SELECT BulkColumn AS OutputFileContents, @OutputFilePath AS OutputFilePath FROM OPENROWSET(BULK N' + QUOTENAME(@OutputFilePath, N'''') + N', SINGLE_NCLOB) AS d'
+	SELECT TOP (1) @OutputFilePath = @LogDirectory + subdir
+	FROM @FilesList
+	WHERE subdir LIKE @OutputFilePath
+	ORDER BY subdir DESC
+
+	--SELECT @OutputFilePath AS [@OutputFilePath], @LogDirectory AS [@LogDirectory]
+END
+
+
+SET @cmd = N'SELECT BulkColumn AS OutputFileContents, @OutputFilePath AS OutputFilePath FROM OPENROWSET(BULK N'''
+		+ REPLACE(@OutputFilePath, N'''', N'''''') + N''', SINGLE_NCLOB) AS d'
 
 RAISERROR(N'Output File Path: %s', 0, 1, @OutputFilePath);
 RAISERROR(N'Query Command: %s', 0, 1, @cmd);
 
 IF @OutputFilePath IS NOT NULL AND @cmd IS NOT NULL
 EXEC sp_executesql @cmd, N'@OutputFilePath nvarchar(max)', @OutputFilePath;
+
+SET NOEXEC OFF;
