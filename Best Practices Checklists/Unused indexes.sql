cb4d33e19072174ed174e3864275ded3de29dd93
Eitan Blumin <eitan@madeiradata.com>
2022-05-09 16:22:52 +0300

    Update Unused indexes.sql
    - added parameters: `@MinimumUserUpdates`, `@MinimumTableCreationDays`
    - case sensitivity bug fix
    - database iteration bug fix (added cursor)
    - added server uptime to output messages
    - remediation commands improvement

--- Best Practices Checklists/Unused indexes.sql
+++ Best Practices Checklists/Unused indexes.sql
@@ -4,80 +4,92 @@ Description: Use this script to retrieve all unused indexes across all of your d
 The data returned includes various index usage statistics and a corresponding drop command.
 Supports both on-premise instances, as well as Azure SQL Databases.
 */
+DECLARE
+	 @MinimumRowsInTable INT = 200000
+	,@MinimumUserUpdates INT = 100
+	,@MinimumTableCreationDays INT = 30
+
 SET NOCOUNT ON;
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-DECLARE @MinimumRowsInTable INT = 200000;
 DECLARE @CMD NVARCHAR(MAX);
-SET @CMD = N'PRINT DB_NAME();
-SELECT
- db_name() AS DBNAme,
+SET @CMD = N'SELECT
+ DB_NAME() AS DBName,
  OBJECT_SCHEMA_NAME(indexes.object_id) as SchemaName,
  OBJECT_NAME(indexes.object_id) AS Table_name,
  indexes.name AS Index_name,
  SUM(partitions.rows),
  SUM(partition_stats.reserved_page_count) * 8,
- STATS_DATE(indexes.object_id, indexes.index_id) StatsDate,
+ SUM(ISNULL(usage_stats.user_updates, 0) + ISNULL(usage_stats.system_updates, 0)),
  tables.create_date,
- ISNULL(usage_stats.user_updates, 0) + ISNULL(usage_stats.system_updates, 0)
-FROM
- sys.indexes
-INNER JOIN sys.tables
- ON indexes.object_id = tables.object_id 
- AND tables.create_date < DATEADD(dd, -30, GETDATE())
+ STATS_DATE(indexes.object_id, indexes.index_id) StatsDate
+FROM sys.indexes
+INNER JOIN sys.tables ON indexes.object_id = tables.object_id 
+INNER JOIN sys.partitions ON indexes.object_id = partitions.object_id AND indexes.index_id = partitions.index_id
+LEFT JOIN sys.dm_db_index_usage_stats AS usage_stats ON indexes.index_id = usage_stats.index_id AND usage_stats.object_id = indexes.object_id
+LEFT JOIN sys.dm_db_partition_stats AS partition_stats ON indexes.index_id = partition_stats.index_id AND partition_stats.object_id = indexes.object_id
+WHERE
+ ISNULL(usage_stats.user_updates, 0) + ISNULL(usage_stats.system_updates, 0) > ' + CAST(@MinimumUserUpdates AS NVARCHAR(MAX)) + N'
+ AND ISNULL(usage_stats.user_lookups,0) = 0
+ AND ISNULL(usage_stats.user_seeks,0) = 0
+ AND ISNULL(usage_stats.user_scans,0) = 0
+ AND tables.create_date < DATEADD(dd, -' + CAST(@MinimumTableCreationDays AS NVARCHAR(MAX)) + N', GETDATE())
  AND tables.is_ms_shipped = 0
  AND indexes.index_id > 1
  AND indexes.is_primary_key = 0
  AND indexes.is_unique = 0
  AND indexes.is_disabled = 0
  AND indexes.is_hypothetical = 0
-INNER JOIN sys.partitions
- ON indexes.object_id = partitions.object_id
- AND indexes.index_id = partitions.index_id
-LEFT JOIN sys.dm_db_index_usage_stats AS usage_stats
- ON indexes.index_id = usage_stats.index_id AND usage_stats.OBJECT_ID = indexes.OBJECT_ID
-LEFT JOIN sys.dm_db_partition_stats AS partition_stats
- ON indexes.index_id = partition_stats.index_id AND partition_stats.OBJECT_ID = indexes.OBJECT_ID
-WHERE
- usage_stats.user_updates > 100
- AND ISNULL(usage_stats.system_seeks,0) = 0
- AND ISNULL(usage_stats.user_seeks,0) = 0
- AND ISNULL(usage_stats.user_scans,0) = 0
 GROUP BY
  indexes.object_id,
  tables.create_date,
  indexes.index_id,
- indexes.name,
- usage_stats.user_seeks,
- usage_stats.user_scans,
- usage_stats.user_updates,
- usage_stats.system_updates
+ indexes.name
 HAVING
  SUM(partitions.rows) > ' + CAST(@MinimumRowsInTable AS NVARCHAR(MAX))
 
 IF OBJECT_ID('tempdb..#tmp') IS NOT NULL DROP TABLE #tmp;
 CREATE TABLE #tmp (DBName SYSNAME, SchemaName SYSNAME, TableName SYSNAME, IndexName SYSNAME NULL, RowsCount INT, IndexSizeKB INT, UpdatesCount INT NULL, TableCreatedDate DATETIME NULL, LastStatsDate DATETIME);
 
-IF CONVERT(int, SERVERPROPERTY('EngineEdition')) <> 5
+DECLARE @CurrDB sysname, @spExecuteSql nvarchar(1000), @InstanceStartTime datetime;
+
+IF OBJECT_ID('sys.dm_os_sys_info') IS NOT NULL
 BEGIN
-	SET @CMD = N'
-IF EXISTS (SELECT * FROM sys.databases WHERE database_id > 4 AND HAS_DBACCESS([name]) = 1 AND state_desc = ''ONLINE'' AND DATABASEPROPERTYEX([name], ''Updateability'') = ''READ_WRITE'')
-BEGIN
- USE [?];' + @CMD + N'
-END'
-	EXEC sp_executesql N'IF (SELECT sqlserver_start_time FROM sys.dm_os_sys_info) < DATEADD(dd,-14,GETDATE())
-BEGIN
- INSERT INTO #tmp(DBName, SchemaName, TableName, IndexName, RowsCount, IndexSizeKB, LastStatsDate, TableCreatedDate, UpdatesCount)
- EXEC sp_MSforeachdb @CMD 
-END', N'@CMD nvarchar(max)', @CMD;
-END
-ELSE
-BEGIN
-	EXEC sp_executesql @CMD;
+	SELECT @InstanceStartTime = sqlserver_start_time
+	FROM sys.dm_os_sys_info
+	OPTION (RECOMPILE);
+
+	PRINT N'SQL Server instance "' + @@SERVERNAME + N'" is up since: ' + CONVERT(nvarchar(25), @InstanceStartTime, 121);
 END
 
+DECLARE DBs CURSOR
+LOCAL FAST_FORWARD
+FOR
+SELECT [name]
+FROM sys.databases
+WHERE HAS_DBACCESS([name]) = 1
+AND database_id > 4
+AND state_desc = 'ONLINE'
+AND DATABASEPROPERTYEX([name], 'Updateability') = 'READ_WRITE'
+
+OPEN DBs;
+
+WHILE 1=1
+BEGIN
+	FETCH NEXT FROM DBs INTO @CurrDB;
+	IF @@FETCH_STATUS <> 0 BREAK;
+	SET @spExecuteSql = QUOTENAME(@CurrDB) + N'..sp_executesql'
+
+	INSERT INTO #tmp
+	EXEC @spExecuteSql @CMD;
+	RAISERROR(N'Database "%s": %d unused indexes.',0,1, @CurrDB,@@ROWCOUNT) WITH NOWAIT;
+END
+
+CLOSE DBs;
+DEALLOCATE DBs;
+
 SELECT *,
-DisableCmd = N'USE ' + QUOTENAME(DBName) + N'; ALTER INDEX ' + QUOTENAME(IndexName) + N' ON ' + QUOTENAME(SchemaName) + N'.' + QUOTENAME(TableName) + N' DISABLE;',
-DisableIfActiveCmd = N'USE ' + QUOTENAME(DBName) + N'; IF INDEXPROPERTY(OBJECT_ID(''' + QUOTENAME(SchemaName) + N'.' + QUOTENAME(TableName) + N'''), ''' + IndexName + N''', ''IsDisabled'') = 0 ALTER INDEX ' + QUOTENAME(IndexName) + N' ON ' + QUOTENAME(SchemaName) + N'.' + QUOTENAME(TableName) + N' DISABLE;',
-DropCmd = N'USE ' + QUOTENAME(DBName) + N'; DROP INDEX ' + QUOTENAME(IndexName) + N' ON ' + QUOTENAME(SchemaName) + N'.' + QUOTENAME(TableName) + N';'
+DisableCmd = N'USE ' + QUOTENAME(DBName) + N'; IF INDEXPROPERTY(OBJECT_ID(''' + QUOTENAME(SchemaName) + N'.' + QUOTENAME(TableName) + N'''), ''' + IndexName + N''', ''IsDisabled'') = 0 ALTER INDEX ' + QUOTENAME(IndexName) + N' ON ' + QUOTENAME(SchemaName) + N'.' + QUOTENAME(TableName) + N' DISABLE;',
+DropCmd = N'USE ' + QUOTENAME(DBName) + N'; IF INDEXPROPERTY(OBJECT_ID(''' + QUOTENAME(SchemaName) + N'.' + QUOTENAME(TableName) + N'''), ''' + IndexName + N''', ''IndexID'') IS NOT NULL DROP INDEX ' + QUOTENAME(IndexName) + N' ON ' + QUOTENAME(SchemaName) + N'.' + QUOTENAME(TableName) + N';'
 FROM #tmp
-ORDER BY DBName ASC, IndexSizeKB DESC, RowsCount DESC
+ORDER BY
+    DBName ASC, IndexSizeKB DESC, RowsCount DESC
