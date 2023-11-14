2291e65e7ceac4aba4dc83e603231f5bd24dbb50
Eitan Blumin <eitan@madeiradata.com>
2023-11-14 07:08:22 +0200

    aggregation fix per table

--- Monitoring Scripts/TempDB Space Utilization Check.sql
+++ Monitoring Scripts/TempDB Space Utilization Check.sql
@@ -54,15 +54,18 @@ GO
 -- Temp Table Space Utilization Stats
 SELECT
 	tb.name			    AS [Temporary table name]
-      , stt.row_count		    AS [Number of rows]
-      , stt.used_page_count * 8	    AS [Used space (KB)]
-      , stt.reserved_page_count * 8 AS [Reserved space (KB)]
+      , SUM(stt.row_count)		    AS [Number of rows]
+      , SUM(stt.used_page_count) * 8	    AS [Used space (KB)]
+      , SUM(stt.reserved_page_count) * 8 AS [Reserved space (KB)]
+	  , tb.create_date AS [Create Date]
+	  --, DropCommand = CONCAT(N'DROP TABLE ', tb.name)
 FROM
 	tempdb.sys.partitions		      AS prt
 INNER	JOIN tempdb.sys.dm_db_partition_stats AS stt ON prt.partition_id = stt.partition_id
 							     AND prt.partition_number = stt.partition_number
 INNER	JOIN tempdb.sys.tables		      AS tb ON stt.object_id = tb.object_id
-ORDER BY [Reserved space (KB)] DESC;
+GROUP BY tb.name, tb.create_date
+ORDER BY [Create Date] ASC, [Reserved space (KB)] DESC;
 GO
 
 -- Session Usage of TempDB
