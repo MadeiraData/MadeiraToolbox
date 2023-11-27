e4e541867892e766bef16c856c32a5e42c49b11d
Eitan Blumin <eitan@madeiradata.com>
2023-11-27 19:27:01 +0200

    Update create_database_snapshot.sql

--- Utility Scripts/create_database_snapshot.sql
+++ Utility Scripts/create_database_snapshot.sql
@@ -4,15 +4,15 @@ DECLARE
 
 DECLARE @CMD NVARCHAR(MAX), @SnapshotName SYSNAME;
 
-SET @SnapshotName = @CurrDB + '_snapshot_' + CONVERT(nvarchar, GETDATE(), 112) + REPLACE(CONVERT(nvarchar, GETDATE(), 114),':','');
+SET @SnapshotName = @CurrDB + '_snapshot_' + CONVERT(nvarchar(25), GETDATE(), 112) + REPLACE(CONVERT(nvarchar(25), GETDATE(), 114),':','');
 
 SELECT @CMD = ISNULL(@CMD + N',
 ', N'') + N'(NAME = ' + QUOTENAME(name) + N'
-, FILENAME = ' + QUOTENAME(LEFT(physical_name, LEN(physical_name) - CHARINDEX('\', REVERSE(physical_name)) + 1)
-+ @SnapshotName + N'_' + name + '.ss', N'''')
-+ N')'
+	, FILENAME = ''' + LEFT(physical_name, LEN(physical_name) - CHARINDEX('\', REVERSE(physical_name)) + 1)
++ @SnapshotName + N'_' + name + '.ss'
++ N''')'
 FROM sys.master_files
-WHERE type <> 1
+WHERE type = 0
 AND database_id = DB_ID(@CurrDB)
 
 SELECT
