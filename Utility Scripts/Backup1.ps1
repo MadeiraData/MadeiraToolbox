eb37f37c22ce7c7d059a2d2b16ae8ee6ec05ff9a
GeriReshef <GeriReshef@GMail.com>
2024-01-06 19:46:42 +0200

    PowerShell scripts for Backup & Restore

---
+++ Utility Scripts/Backup1.ps1
@@ -0,0 +1,8 @@
+﻿$MyServer = "localhost"
+$MyFolder = "C:\TEMP\Backup"
+$DBs = @("Try001","Try002")
+foreach ($DB in $DBs) {
+    Write-Host $DB
+    $date = Get-Date -format "yyyyMMdd_hhmmss"
+    Backup-SqlDatabase -ServerInstance $MyServer -Database $DB -BackupFile $MyFolder\$MyServer-$DB-Full-$date.bak
+}
---
+++ Utility Scripts/Restore2.ps1
@@ -0,0 +1,27 @@
+﻿$BackupFolder="C:\temp\Backup\"
+$NewDataPath="c:\Temp\Data\" #The Data files directory (for the new DB)
+$NewLogPath="c:\Temp\Log\" #The Log file directory (for the new DB)
+$DBs = Get-ChildItem $BackupFolder -Filter *.bak | Out-GridView -OutputMode Multiple
+foreach ($File in $DBs) {
+    Write-Host $File
+    $dbname = $File.Name.replace('.bak','') #The new DB name
+    $relocate = @() #This variable (for the Restore-SqlDatabase) is initialized as a list (and not an empty string), for the "With Move" part of the Restore command
+    $OutputFile="C:\temp\$dbname.sql" #A script file will be created
+    $BackupFile=$BackupFolder+$File.Name
+    $dbfiles = Invoke-Sqlcmd -ServerInstance localhost -Database tempdb -Query "Restore FileListOnly From Disk='$BackupFile';" #The Restore FileListOnly output is inserted into a variable
+    foreach($dbfile in $dbfiles){
+      $DbFileName = $dbfile.PhysicalName | Split-Path -Leaf
+      if($dbfile.Type -eq 'L'){
+        $newfile = Join-Path -Path $NewLogPath -ChildPath $DbFileName #The Log part of the "With Move"
+      } else {
+        $newfile = Join-Path -Path $NewDataPath -ChildPath $DbFileName #The Data parts of the "With Move"
+      }
+      $relocate += New-Object Microsoft.SqlServer.Management.Smo.RelocateFile ($dbfile.LogicalName,$newfile)
+    }
+    Restore-SqlDatabase -ServerInstance localhost `
+        -Database $dbname `
+        -RelocateFile $relocate `
+        -BackupFile "$BackupFile" `
+        -RestoreAction Database #`
+        #-Script | Out-File $OutputFile #An optional parameter: instead of executing the command, it will be exported into a script file.
+}
